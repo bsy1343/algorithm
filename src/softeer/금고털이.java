06d22309e14f9655be1371835f538056755132ae
백상열 <bsy1343@naver.com>
2025-02-09 01:09:43 +0900

    :art: 드궤제

--- src/softeer/금고털이.java
+++ src/softeer/금고털이.java
@@ -1,7 +1,49 @@
 package src.softeer;
 
+
+import java.io.*;
+import java.util.*;
+
+// https://softeer.ai/practice/6288
 public class 금고털이 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int Result = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+
+            map.put(g, map.getOrDefault(g, 0) + w);
+        }
+
+        List<int[]> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
+            list.add(new int[]{mapEntry.getKey(), mapEntry.getValue()});
+        }
+
+        // 값으로 내림차순
+        Collections.sort(list, (o1, o2) -> o2[0] - o1[0]);
+
+        for (int[] arr : list) {
+            if (K >= arr[1]) {
+                K = K - arr[1];
+                Result += arr[0] * arr[1];
+            } else {
+                // 가방의 무게보다 남은 보석의 무게가 많기 때문의 가방이 있는 만큼의 무게만큼 값을 매기고 환산하여 리턴
+                Result += K * arr[0];
+                break;
+            }
+        }
+        System.out.println(Result);
     }
 }
---
+++ src/tip/listSort.java
@@ -0,0 +1,52 @@
+package src.tip;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class listSort {
+    public static void main(String[] args) {
+        // 리스트 생성
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 3});
+        list.add(new int[]{2, 1});
+        list.add(new int[]{3, 2});
+
+        // 첫 번째 수 기준 오름차순
+        Collections.sort(list, (a, b) -> a[0] - b[0]);
+        for(int[] arr : list) {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+        // 첫 번째 수 기준 내림차순
+        Collections.sort(list, (a, b) -> b[0] - a[0]);
+        for(int[] arr : list) {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+        // 두 번째 수 기준 오름차순
+        Collections.sort(list, (a, b) -> a[1] - b[1]);
+        for(int[] arr : list) {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+        // 두 번째 수 기준 내림차순
+        Collections.sort(list, (a, b) -> b[1] - a[1]);
+        for(int[] arr : list) {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+    }
+}

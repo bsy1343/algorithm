e22335edc54ec5e87059131d6a6d1ecffa8089e4
백상열 <bsy1343@naver.com>
2025-02-09 01:57:02 +0900

    :art: 드궤제

--- src/softeer/금고털이.java
+++ src/softeer/금고털이.java
@@ -28,19 +28,19 @@ public class 금고털이 {
 
         List<int[]> list = new ArrayList<>();
         for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
-            list.add(new int[]{mapEntry.getKey(), mapEntry.getValue()});
+            list.add(new int[]{mapEntry.getValue(), mapEntry.getKey()});
         }
 
         // 값으로 내림차순
-        Collections.sort(list, (o1, o2) -> o2[0] - o1[0]);
+        Collections.sort(list, (o1, o2) -> o2[1] - o1[1]);
 
         for (int[] arr : list) {
-            if (K >= arr[1]) {
-                K = K - arr[1];
+            if (K >= arr[0]) {
+                K = K - arr[0];
                 Result += arr[0] * arr[1];
             } else {
                 // 가방의 무게보다 남은 보석의 무게가 많기 때문의 가방이 있는 만큼의 무게만큼 값을 매기고 환산하여 리턴
-                Result += K * arr[0];
+                Result += K * arr[1];
                 break;
             }
         }

9f4f595ab530766903e00ca95f3c2d627e4aed2c
백상열 <bsy1343@naver.com>
2025-02-22 00:12:44 +0900

    :art: 드궤제

--- Softeer/Main.java
+++ Softeer/Main.java
@@ -9,29 +9,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        String[] arr = {"1110111", "0010010", "0111101", "0111011", "1011010", "1101011", "1101111", "1110010", "1111111", "1111011"};
+        String empty = "0000000";
 
-        Map<String, int[]> map = new TreeMap<>();
-        //Map<String, int[]> map = new TreeMap<>(Collections.reverseOrder());
-        for (int i = 0; i < N; i++) {
-            map.put(br.readLine(), new int[18]);
-        }
+        int t = Integer.parseInt(st.nextToken());
+        int[] a = new int[5];
+        int[] b = new int[5];
 
-        for (int i = 0; i < M; i++) {
+        for (int z = 0; z < t; z++) {
             st = new StringTokenizer(br.readLine());
-            String name = st.nextToken();
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            for (int j = start; j < end; j++) {
-                map.get(name)[j] = 1;
+            String sa = String.format("%5s", st.nextToken());
+            String sb = String.format("%5s", st.nextToken());
+
+            for (int i = 0; i < 5; i++) {
+                a[i] = sa.charAt(i) == ' ' ? -1 : sa.charAt(i) - '0';
+                b[i] = sb.charAt(i) == ' ' ? -1 : sb.charAt(i) - '0';
             }
+            // System.out.println(Arrays.toString(a) + " : " + Arrays.toString(b));
+
+            int cnt = 0;
+            for (int i = 0; i < 5; i++) {
+                if (a[i] != b[i]) {
+                    String a_value = a[i] == -1 ? empty : arr[a[i]];
+                    String b_value = b[i] == -1 ? empty : arr[b[i]];
+                    for (int j = 0; j < 7; j++) {
+                        if (a_value.charAt(j) != b_value.charAt(j)) {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            System.out.println(cnt);
         }
-
-        for (Map.Entry<String, int[]> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " : " + Arrays.toString(entry.getValue()));
-        }
-
-
     }
 }

db4f84888b6c9a3389b46e4f846f4ad8dd231cc5
백상열 <bsy1343@naver.com>
2025-02-22 18:29:36 +0900

    :art: 드궤제

--- Softeer/Lv2/팔단변속기.java
+++ Softeer/Lv2/팔단변속기.java
@@ -23,6 +23,14 @@ public class 팔단변속기 {
             result = "descending";
         }
 
+
+//        String result = "mixed";
+//        if (st.contains("1 2 3 4 5 6 7 8")) {
+//            result = "ascending";
+//        } else if (st.contains("8 7 6 5 4 3 2 1")) {
+//            result = "descending";
+//        }
+
         System.out.println(result);
     }
 }

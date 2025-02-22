c4dfa170bbe3f47595628cba862442895ab9d67f
백상열 <bsy1343@naver.com>
2025-02-22 17:28:09 +0900

    :art: 드궤제

--- Softeer/Lv2/지도자동구축.java
+++ Softeer/Lv2/지도자동구축.java
@@ -1,21 +1,20 @@
 package Softeer.Lv2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 // https://softeer.ai/practice/6280
 public class 지도자동구축 {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
-        int D = Integer.parseInt(br.readLine());
-        int K = 2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        // 2, 3, 5, 9, 17 ...
-        for (int i = 0; i < D; i++) {
+        int K = 2;
+        // K = 2, 3, 5, 9, 17, 33
+        for (int i = 0; i < N; i++) {
             K = K * 2 - 1;
         }
         System.out.println(K * K);
+        sc.close();
     }
 }

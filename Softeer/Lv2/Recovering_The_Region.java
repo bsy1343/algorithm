e980a540d2437550eabf7818c507ef58f52c3cfa
백상열 <bsy1343@naver.com>
2025-02-24 02:11:43 +0900

    :art: 드궤제

--- Softeer/Lv2/Recovering_The_Region.java
+++ Softeer/Lv2/Recovering_The_Region.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 // https://softeer.ai/practice/9497
 public class Recovering_The_Region {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
+        int n = sc.nextInt();
         int cnt = 1;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (j == N-1) {
-                    System.out.print(cnt);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == n-1) {
+                    System.out.println(cnt++);
                 } else {
                     System.out.print(cnt + " ");
                 }
             }
-            cnt++;
-            System.out.println();
         }
         sc.close();
     }

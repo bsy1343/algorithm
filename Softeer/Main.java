c95be42dfa129712001103992e57c91e1c096077
백상열 <bsy1343@naver.com>
2025-02-27 00:56:39 +0900

    :art: 드궤제

--- Softeer/Main.java
+++ Softeer/Main.java
@@ -1,46 +1,79 @@
 package Softeer;
 
-import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-        String[] arr = {"1110111", "0010010", "0111101", "0111011", "1011010", "1101011", "1101111", "1110010", "1111111", "1111011"};
-        String empty = "0000000";
-
-        int t = Integer.parseInt(st.nextToken());
-        int[] a = new int[5];
-        int[] b = new int[5];
-
-        for (int z = 0; z < t; z++) {
-            st = new StringTokenizer(br.readLine());
-            String sa = String.format("%5s", st.nextToken());
-            String sb = String.format("%5s", st.nextToken());
-
-            for (int i = 0; i < 5; i++) {
-                a[i] = sa.charAt(i) == ' ' ? -1 : sa.charAt(i) - '0';
-                b[i] = sb.charAt(i) == ' ' ? -1 : sb.charAt(i) - '0';
-            }
-            // System.out.println(Arrays.toString(a) + " : " + Arrays.toString(b));
-
-            int cnt = 0;
-            for (int i = 0; i < 5; i++) {
-                if (a[i] != b[i]) {
-                    String a_value = a[i] == -1 ? empty : arr[a[i]];
-                    String b_value = b[i] == -1 ? empty : arr[b[i]];
-                    for (int j = 0; j < 7; j++) {
-                        if (a_value.charAt(j) != b_value.charAt(j)) {
-                            cnt++;
-                        }
-                    }
-                }
-            }
-            System.out.println(cnt);
+    static class Node {
+        int x;
+        int y;
+        int s;
+        Node (int x, int y, int s) {
+            this.x = x;
+            this.y = y;
+            this.s = s;
         }
+    }
+
+    static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int n, m, answer;
+    static int[][] maps, visited;
+
+    public int solution(int[][] maps) {
+        this.answer = -1;
+        this.maps = maps;
+        this.n = maps.length;
+        this.m = maps[0].length;
+        this.visited = new int[n][m];
+
+        bfs(0, 0, maps[0][0]);
+
+        return answer;
+    }
+
+
+    private void bfs(int x, int y, int s) {
+        Queue<Node> q = new LinkedList();
+        visited[x][y] = 1;
+        q.add(new Node(x, y, s));
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+
+            if (now.x == n-1 && now.y == m-1) {
+                answer = now.s;
+                return;
+            }
+
+            for (int i = 0; i < direction.length; i++) {
+                int dx = direction[i][0] + now.x;
+                int dy = direction[i][1] + now.y;
+
+                if (dx < 0 || dy < 0 || dx >= n || dy >= m || maps[dx][dy] == 0 ||visited[dx][dy] == 1) {
+                    continue;
+                }
+
+                visited[dx][dy] = 1;
+                q.add(new Node(dx, dy, now.s+1));
+            }
+
+        }
+    }
+
+    // 테스트를 위한 main 메서드 추가
+    public static void main(String[] args) {
+        // 테스트용 맵 생성
+        int[][] testMap = {
+            {1, 0, 1, 1, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 1, 1, 1},
+            {1, 1, 1, 0, 1},
+            {0, 0, 0, 0, 1}
+        };
+
+        Main sol = new Main();
+        int result = sol.solution(testMap);
+        System.out.println("최단 경로의 길이: " + result);
     }
 }

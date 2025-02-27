46a4c5f0bca820b5da64c41254cb6f30a1f0a60a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-02-28 02:42:42 +0900

    [Gold IV] Title: 테트로미노, Time: 512 ms, Memory: 33980 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,167 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M, Max;
+	static int Map[][];
+	static int[][][] Direction = {
+		{
+			{1, 1, 1, 1},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0}
+		},
+		
+		{
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		
+		{
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 1, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 1, 0, 0},
+			{0, 1, 0, 0},
+			{1, 1, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 1, 0},
+			{1, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 1, 0},
+			{0, 0, 1, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 0, 0, 0},
+			{1, 1, 1, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 0, 1, 0},
+			{1, 1, 1, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		
+		{
+			{1, 0, 0, 0},
+			{1, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 0, 0},
+			{0, 1, 1, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 1, 1, 0},
+			{1, 1, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		
+		{
+			{1, 0, 0, 0},
+			{1, 1, 0, 0},
+			{1, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 1, 0, 0},
+			{1, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{1, 1, 1, 0},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		},
+		{
+			{0, 1, 0, 0},
+			{1, 1, 1, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0}
+		}
+	};
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); 
+		M = Integer.parseInt(st.nextToken());
+		Map = new int[N+3][M+3];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				Map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Max = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				Max = Math.max(calc(i, j), Max);
+			}
+		}
+		System.out.println(Max);
+	}
+
+	static int calc(int x, int y) {
+		int max = Integer.MIN_VALUE;
+		for (int k = 0; k < Direction.length; k++) {
+			int sum = 0;
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (Direction[k][i][j] == 1) {
+						sum += Map[x+i][y+j];
+					}
+				}
+			}
+			max = Math.max(max, sum);
+		}
+		return max; 
+	}
+}

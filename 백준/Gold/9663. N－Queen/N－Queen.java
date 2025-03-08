2b30ae60d33aa11f44c638b33277dffc27b697f0
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-09 05:38:22 +0900

    [Gold IV] Title: N-Queen, Time: 5144 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,99 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
 
-    static void input() {
-        N = scan.nextInt();
-        col = new int[N + 1];
-    }
-
-    static int N, ans;
-    static int[] col;
-
-    static boolean attackable(int r1, int c1, int r2, int c2) {
-        if (c1 == c2) return true;
-        if (r1 - c1 == r2 - c2) return true;
-        if (r1 + c1 == r2 + c2) return true;
-        return false;
-    }
-
-    static void rec_func(int row) {
-        if (row == N + 1) {
-            ans++;
-        } else {
-            for (int c = 1; c <= N; c++) {
-                boolean possible = true;
-                // valid check (row, c)
-                for (int i=1;i<=row-1;i++){
-                    // (i, col[i])
-                    if (attackable(row, c, i, col[i])){
-                        possible = false;
-                        break;
-                    }
-                }
-                if (possible) {
-                    col[row] = c;
-                    rec_func(row + 1);
-                    col[row] = 0;
-                }
-            }
-        }
-    }
+    static int[] map;
+    static int n, q, answer;
 
     public static void main(String[] args) {
-        input();
-        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
-        rec_func(1);
-        System.out.println(ans);
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        q = n;
+        map = new int[n];
+
+        dfs(0);
+
+        System.out.print(answer);
     }
 
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
+    static void dfs(int row) {
+        if (row == n) {
+            answer++;
+        } else {
+            for (int col = 0; col < n; col++) {
+                if (isAttack(row, col)) {
+                    map[row] = col;
+                    dfs(row + 1);
+                    map[row] = 0;
                 }
             }
-            return st.nextToken();
         }
+    }
 
-        int nextInt() {
-            return Integer.parseInt(next());
+    static boolean isAttack (int row, int col) {
+        for (int i = 0; i < row; i++) {
+            // 같은 행인지 검사
+            if (map[i] == col) return false;
+            // ↘️(우하향) 대각선 검사
+            if (row + col ==  i + map[i]) return false;
+            // ↖️(좌상향) 대각선 검사
+            if (row - col ==  i - map[i]) return false;
+            // if (Math.abs(row - i) == Math.abs(col - map[i])) 대각선검사
         }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
+        return true;
     }
 }

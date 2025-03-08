cdf6b8066637b3d258574d343604f809666249e9
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-09 04:40:30 +0900

    [Gold IV] Title: N-Queen, Time: 5532 ms, Memory: 15104 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,49 +1,99 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	static int N, Result;
-	static int[] Map;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		Map = new int[N];
-		backTracking(0);
-		System.out.println(Result);
-	}
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-	static void backTracking(int row) {
-		if (row == N) {
-			Result++;
-			return;
-		}
-		
-		for (int i = 0; i < N; i++) {
-			if (isPossible(row, i)) {
-				Map[row] = i;
-				backTracking(row+1);
-				Map[row] = 0;
-				
-			}
-		}
-	}
-	
-	static boolean isPossible(int row, int col) {
-		for (int i = 0; i < row; i++) {
-			/*
-			if (row == i) {
-				return false;
-			}
-			*/
-			if (Map[i] == col) {
-				return false;
-			}
-			
-			if (Math.abs(row - i) == Math.abs(Map[i] - col)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    static void input() {
+        N = scan.nextInt();
+        col = new int[N + 1];
+    }
+
+    static int N, ans;
+    static int[] col;
+
+    static boolean attackable(int r1, int c1, int r2, int c2) {
+        if (c1 == c2) return true;
+        if (r1 - c1 == r2 - c2) return true;
+        if (r1 + c1 == r2 + c2) return true;
+        return false;
+    }
+
+    static void rec_func(int row) {
+        if (row == N + 1) {
+            ans++;
+        } else {
+            for (int c = 1; c <= N; c++) {
+                boolean possible = true;
+                // valid check (row, c)
+                for (int i=1;i<=row-1;i++){
+                    // (i, col[i])
+                    if (attackable(row, c, i, col[i])){
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible) {
+                    col[row] = c;
+                    rec_func(row + 1);
+                    col[row] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
+        rec_func(1);
+        System.out.println(ans);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }

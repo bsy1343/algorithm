34ab9be5465bce047bad1089debf87c66079b82a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-14 03:08:46 +0900

    [Gold IV] Title: 고냥이, Time: 268 ms, Memory: 21480 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -3,28 +3,24 @@ import java.util.*;
 
 public class Main {
 
+    static int n, kind;
     static int[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
+        kind = 0;
         visited = new int[26];
         String str = sc.next();
         char[] arr = str.toCharArray();
 
-        int len = arr.length, kind, answer = 0;
+        int len = arr.length, answer = 0;
         for (int l = 0, r = 0; r < len; r++) {
             // r번째 문자를 오른쪽에 추가
             add(arr[r]);
 
             // 불가능하면, 가능할때까지 l을 이동
-            while(true) {
-                kind = 0;
-                for (int i = 0; i < 26; i++) {
-                    if (visited[i] != 0) kind++;
-                }
-                if (kind <= n) break;
-                erase(arr[l]);
-                l++;
+            while(kind > n) {
+                erase(arr[l++]);
             }
 
             // 정답갱신
@@ -37,10 +33,20 @@ public class Main {
     // x라는 알파벳 추가
     static void add(char x) {
         visited[x-'a']++;
+
+        // 새롭게 나타난 알파벳이라는 뜻
+        if (visited[x-'a'] == 1) {
+            kind++;
+        }
     }
 
     // x라는 알파벳 제거
     static void erase(char x) {
         visited[x-'a']--;
+
+        // 인식해야 하는 알파벳에서 빠지는 순간
+        if (visited[x-'a'] == 0) {
+            kind--;
+        }
     }
 }

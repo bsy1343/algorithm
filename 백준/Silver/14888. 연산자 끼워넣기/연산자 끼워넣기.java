6a28b269b4cfd46a10e8bf21e6d2ea189fd2e485
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-16 02:52:14 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,51 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, min, max;
-    static int[] nums, operators;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        nums = new int[n];
-        operators = new int[5];
+    static int n, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    static int[] arr, operator;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        operator = new int[4];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operators[i] = sc.nextInt();
+            operator[i] = Integer.parseInt(st.nextToken());
         }
 
-        min = Integer.MAX_VALUE;
-        max = Integer.MIN_VALUE;
-
-        dfs(0, nums[0]);
+        dfs(0, arr[0]);
 
         System.out.println(max);
         System.out.println(min);
     }
 
-    static void dfs(int k, int result) {
-        if (k == n-1) {
+    static void dfs (int index, int result) {
+        if (index == n-1) {
             min = Math.min(min, result);
             max = Math.max(max, result);
         } else {
             for (int i = 0; i < 4; i++) {
-                if (operators[i] > 0) {
-                    operators[i]--;
-                    dfs(k + 1, calculator(result, nums[k+1], i));
-                    operators[i]++;
+                if (operator[i] > 0) {
+                    operator[i]--;
+                    dfs(index + 1, calc(result, arr[index+1], i));
+                    operator[i]++;
                 }
-
             }
         }
     }
 
-    static int calculator(int value, int cmd, int operator) {
+    static int calc(int value, int cmd, int operator) {
         if (operator == 0) {
             value += cmd;
         } else if (operator == 1) {

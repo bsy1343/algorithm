ab8b8f6a5ebb44467b0cdc03cf1a8dffecefd066
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-16 12:15:36 +0900

    [Silver IV] Title: 카드, Time: 344 ms, Memory: 35296 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -9,36 +9,31 @@ public class Main {
             this.num = num;
             this.cnt = cnt;
         }
-
         @Override
         public int compareTo(Node node) {
-            // 카운트의 내림차순
-            if (cnt != node.cnt) return node.cnt - cnt;
-            // 수의 오름차순
-            return Long.compare(num, node.num);
+            if (this.cnt != node.cnt) return node.cnt - this.cnt;
+            return Long.compare(this.num, node.num);
         }
     }
-    static int n;
-    static int[] arr;
-    static ArrayList<Node> al = new ArrayList();
-    static HashMap<Long, Integer> map = new HashMap();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new int[n+1];
 
+    static int n;
+    static ArrayList<Node> al = new ArrayList();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        HashMap<Long, Integer> map = new HashMap();
         for (int i = 0; i < n; i++) {
-            long num = sc.nextLong();
+            Long num = Long.parseLong(br.readLine());
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (Map.Entry<Long, Integer> map : map.entrySet()) {
-            al.add(new Node(map.getKey(), map.getValue()));
+        for (Map.Entry<Long, Integer> entry : map.entrySet()) {
+            al.add(new Node(entry.getKey(), entry.getValue()));
         }
 
         Collections.sort(al);
 
-        System.out.print(al.get(0).num);
-        sc.close();
+        System.out.println(al.get(0).num);
     }
 }

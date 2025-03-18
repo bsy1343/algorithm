7be3d63238edd24d26e468bd2c0369cc2181d833
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-18 11:47:08 +0900

    [Silver II] Title: 나무 자르기, Time: 480 ms, Memory: 119336 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -3,40 +3,41 @@ import java.util.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n, m, answer;
-    static int l = 0;
-    static int r = Integer.MIN_VALUE;
+    static int n, m;
     static int[] arr;
-
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         arr = new int[n];
 
+        int l = 0, r = Integer.MIN_VALUE;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             r = Math.max(r, arr[i]);
         }
-        
-        // l은 나무의 최소 높이, r은 나무의 최대 높이 
+
+        int answer = 0;
+        // l은 나무의 최소 높이, r은 나무의 최대 높이
         while (l <= r) {
-            int mid = (l + r) / 2;
+            int mid = ( l + r ) / 2;
+            // determination
             if (determination(mid)) {
                 answer = mid;
                 l = mid + 1;
             } else {
-                r = mid - 1;
+                r = mid -1;
             }
         }
+
         System.out.println(answer);
     }
 
     static boolean determination(int h) {
         // h 높이로 나무들을 잘랐을 떄, M 만큼을 얻을 수 있으면 true, 그렇지 않으면 false를 return하는 함수
+        // 나무의 길이의 범위가 20억이고 sum할 경우 21억이 넘을 가능성이 있기에 int -> long
         long sum = 0;
         for (int i = 0; i < n; i++) {
             if (arr[i] > h) {

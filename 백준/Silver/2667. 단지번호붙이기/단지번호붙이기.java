a898f50be3208b27504ea34ee8edfa9f3e1a1d1f
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-22 01:29:55 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
     static boolean[][] visited;
-    static char[][] map;
+    static int[][] map;
     static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     static ArrayList<Integer> al = new ArrayList();
     static int n, answer;
@@ -12,20 +12,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        map = new char[n][n];
+        map = new int[n][n];
         visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             String str = sc.next();
             for (int j = 0; j < n; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (map[i][j] != '1') continue;
+                if (map[i][j] != 1) continue;
                 if (visited[i][j]) continue;
                 cnt++;
                 answer = 0;
@@ -52,7 +52,7 @@ public class Main {
             int ny = y + direction[i][1];
 
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-            if (map[nx][ny] != '1') continue;
+            if (map[nx][ny] != 1) continue;
             if (visited[nx][ny]) continue;
             dfs(nx, ny);
         }

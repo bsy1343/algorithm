f3bb07baff00b5d2b0dde26eefada2f506ae88e1
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-24 23:20:30 +0900

    [Gold III] Title: 줄 세우기, Time: 456 ms, Memory: 47116 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] indeg = new int[n+1];
+        ArrayList<Integer>[] adj = new ArrayList[n+1];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) adj[i] = new ArrayList();
+
+        for (int i = 1; i <=m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            adj[x].add(y);
+            // indegree 계산하기
+            indeg[y]++;
+        }
+
+        Deque<Integer> q = new LinkedList();
+        // 제일 앞에 "정렬될 수 있는" 정점 찾기
+        for (int i = 1; i <= n; i++) {
+            if (indeg[i] == 0) q.add(i);
+        }
+
+        // 정렬될 수 있는 정점이 있다면?
+        // 1. 정렬 결과에 추가하기
+        // 2. 정점과 연결된 간선 제거하기
+        // 3. 새롭게 "정렬 될 수 있는" 정점 Queue에 추가하기
+        while(!q.isEmpty()) {
+            int x = q.poll();
+            sb.append(x).append(" ");
+
+            for (int y : adj[x]) {
+                indeg[y]--;
+                if (indeg[y] == 0) q.add(y);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}

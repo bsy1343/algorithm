1404a01afcb8e30a0d806e1a6fae50fcaf4d99c1
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-03-26 16:59:27 +0900

    [Gold IV] Title: 최단경로, Time: 904 ms, Memory: 111812 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,95 +1,76 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import org.w3c.dom.Node;
 
 public class Main {
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	
-	static int v, e, k;
-	static int inf = Integer.MAX_VALUE;
-	static int[] dist;
-	static boolean[] visit;
-	static ArrayList<Edge>[] al;
-	
-	
-	static class Edge /**implements Comparable<Edge>**/{
-		int dest;
-		int weight;
-		Edge (int dest, int weight) {
-			this.dest = dest;
-			this.weight = weight;
-		}
-		/**
-		@Override
-		public int compareTo(Edge e) {
-			return this.weight - e.weight;
-		}
-		**/
-		
-	}
-	public static void main(String[] args) throws Exception{
-		st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		k = Integer.parseInt(st.nextToken());
-		
-		dist = new int[v + 1];
-		visit = new boolean[v + 1];
-		
-		al = new ArrayList[v + 1];
-		for (int i = 1; i <= v; i++) {
-			dist[i] = inf;
-			al[i] = new ArrayList<Edge>();
-		}
-		
-		dist[k] = 0;
-		
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			
-			al[u].add(new Edge(v, w));
-		}
-		
-		dijktra(k);
-		
-		for (int i = 1; i <= v; i++) {
-			if (dist[i] == inf) {
-				System.out.println("INF");
-			} else {
-				System.out.println(dist[i]);
-			}
-		}
-	}
-	
-	static void dijktra (int k) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.weight));
-		pq.add(new Edge(k, 0));
-		
-		while (!pq.isEmpty()) {
-			Edge e = pq.poll();
-			
-			if (visit[e.dest] == true) {
-				continue;
-			}
-			
-			visit[e.dest] = true;
-			
-			for (Edge edge: al[e.dest]) {
-				if (visit[edge.dest] == false) {
-					dist[edge.dest] = Math.min(dist[edge.dest], dist[e.dest] + edge.weight);
-					pq.add(new Edge(edge.dest, dist[edge.dest]));
-				}
-			}
-		}
-	}
+
+    static class Edge{
+        int to, weight;
+        Edge (int to, int weight){
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    static class Info{
+        int idx, dist;
+        Info(int idx, int dist) {
+            this.idx = idx;
+            this.dist = dist;
+        }
+    }
+
+    static int v, e, start;
+    static int[] dist;
+    static ArrayList<Edge>[] arrList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(br.readLine());
+
+        dist = new int[v + 1];
+        arrList = new ArrayList[v + 1];
+
+        for (int i = 1; i <= v; i++) arrList[i] = new ArrayList();
+
+        for (int i = 1; i <= e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            arrList[s].add(new Edge(e, w));
+        }
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dijkstra(start);
+
+        for (int i = 1; i <= v; i++) {
+            System.out.println(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]);
+        }
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
+        pq.add(new Info(start, 0));
+        dist[start] = 0;
+
+        while(!pq.isEmpty()) {
+            Info info = pq.poll();
+
+            if (dist[info.idx] < info.dist) continue;
+
+            for (Edge e : arrList[info.idx]) {
+                if (info.dist + e.weight >= dist[e.to]) continue;
+                dist[e.to] = info.dist + e.weight;
+                pq.add(new Info(e.to, dist[e.to]));
+            }
+        }
+
+    }
 }

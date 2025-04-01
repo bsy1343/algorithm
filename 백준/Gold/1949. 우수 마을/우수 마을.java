89f079e81af286fa5199e159e4c87923a513c37e
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-04-02 01:36:41 +0900

    [Gold II] Title: 우수 마을, Time: 220 ms, Memory: 23080 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] num;
+    static int[][] dy;
+    static ArrayList<Integer>[] arrList;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        num = new int[n + 1];
+        dy = new int[n + 1][2];
+        arrList = new ArrayList[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            arrList[i] = new ArrayList();
+        }
+
+        for (int i = 1; i <= n-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arrList[x].add(y);
+            arrList[y].add(x);
+        }
+
+        dfs(1, -1);
+
+        System.out.println(Math.max(dy[1][0], dy[1][1]));
+    }
+
+    static void dfs(int x, int par) {
+        // x를 root로 하는 subtree에서 root를 선택하지 않고서 가능한 최대 주민 수
+        dy[x][0] = 0;
+        // x를 root로 하는 subtree에서 root를 선택하고서 가능한 최대 주민 수
+        dy[x][1] = num[x];
+
+        for (int y : arrList[x]) {
+            if (y == par) continue;
+            dfs(y, x);
+            dy[x][0] += Math.max(dy[y][0], dy[y][1]);
+            dy[x][1] += dy[y][0];
+        }
+
+    }
+}

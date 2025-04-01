726b895b89ec951831c584f5ee4e52c5a85e31a6
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-04-01 22:21:28 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1468 ms, Memory: 89840 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, r, q;
+    static int[] dy;
+    static ArrayList<Integer>[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[n + 1];
+        dy = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = new ArrayList();
+        }
+
+        for (int i = 1; i <= n-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[x].add(y);
+            arr[y].add(x);
+        }
+
+        dfs(r, -1);
+
+        for (int i = 1; i <= q; i++) {
+            int x = Integer.parseInt(br.readLine());
+            System.out.println(dy[x]);
+        }
+    }
+
+    // dy[x]를 계산하는 함수
+    static void dfs(int x, int par) {
+        dy[x] = 1;
+        for (int y : arr[x]) {
+            if (y == par) continue;
+            dfs(y, x);
+            dy[x] += dy[y];
+        }
+    }
+}

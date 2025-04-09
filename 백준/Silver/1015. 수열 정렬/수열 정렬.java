68a926e44b1f11ca9c6a92206ba68a7260afb6f1
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-04-09 17:13:01 +0900

    [Silver IV] Title: 수열 정렬, Time: 124 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/1015. 수열 정렬/수열 정렬.java
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -1,39 +1,46 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static class Node implements Comparable<Node> {
-        int value, index;
-        Node (int value, int index) {
-            this.value = value;
-            this.index = index;
+        int num, idx;
+
+        Node(int num, int idx) {
+            this.num = num;
+            this.idx = idx;
         }
+
         @Override
-        public int compareTo (Node node) {
-            if (value != node.value) return value - node.value;
-            return index - node.index;
+        public int compareTo(Node node) {
+            if (this.num != node.num) return this.num - node.num;
+            return this.idx - node.idx;
         }
     }
-
     static int n;
-    static int[] p;
+    static int[] arr, p;
     static ArrayList<Node> al = new ArrayList();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        p = new int[n];
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            al.add(new Node(sc.nextInt(), i));
+            al.add(new Node(Integer.parseInt(st.nextToken()), i));
         }
 
         Collections.sort(al);
 
         for (int i = 0; i < n; i++) {
-            p[al.get(i).index] = i;
+            arr[al.get(i).idx] = i;
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(p[i] + " ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }

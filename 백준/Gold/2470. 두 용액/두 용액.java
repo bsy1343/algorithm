3372bf46c66b68dff89be48b7e7208634fd8eb00
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-04-15 01:55:08 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28516 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,39 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static int[] arr;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int l = 0, r = n-1, best_sum = Integer.MAX_VALUE, x = 0, y = 0;
-        while (l < r) {
+        int l = 0, r = n -1, max = Integer.MAX_VALUE, x = 0, y = 0;
+        while(l < r) {
             int sum = arr[l] + arr[r];
-            if (Math.abs(sum) < best_sum) {
-                best_sum = Math.abs(sum);
+
+            if (Math.abs(sum) < max) {
+                max = Math.abs(sum);
                 x = arr[l];
                 y = arr[r];
             }
+
             if (sum < 0) {
                 l++;
             } else {
                 r--;
             }
-
         }
-        if (x < y) {
-            System.out.println(x + " " + y);
-        } else {
+        if (x > y) {
             System.out.println(y + " " + x);
+        } else {
+            System.out.println(x + " " + y);
         }
-
     }
 }

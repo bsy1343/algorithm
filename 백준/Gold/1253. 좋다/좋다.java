c82ddf95d1cd043c2fd0fdb02d6061fa9e5b2a32
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-04-16 01:33:41 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,13 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
@@ -17,30 +21,31 @@ public class Main {
 
         int answer = 0;
         for (int i = 0; i < n; i++) {
-            int l = 0, r = n-1, sum = arr[i];
+            int target = arr[i];
 
+            int l = 0, r = n -1;
             while(l < r) {
-                // 자기 자신을 포함하지 않도록 체크
                 if (l == i) {
                     l++;
                     continue;
                 }
+
                 if (r == i) {
                     r--;
                     continue;
                 }
-                int rst = arr[l] + arr[r];
-                if (rst == sum) {
+
+                int sum = arr[l] + arr[r];
+                if (sum == target) {
                     answer++;
                     break;
-                } else if (rst > sum) {
-                    r--;
-                } else if (rst < sum){
+                } else if (sum < target) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }
-
         System.out.println(answer);
     }
 }

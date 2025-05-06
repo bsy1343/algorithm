8e0b8907d4bcdaa6cbb80d734ea4b83aa6f2520a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-05-06 18:45:23 +0900

    [Gold III] Title: ACM Craft, Time: 704 ms, Memory: 238444 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -2,62 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static int t, n, k, w;
+    static int[] arr, indeg, arr_done;
+    static ArrayList<Integer>[] al;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int t = Integer.parseInt(br.readLine());
+        t = Integer.parseInt(br.readLine());
 
         for (int test_case = 0; test_case < t; test_case++) {
             st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
 
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            int[] arr = new int[n+1];
-            int[] arr_done = new int[n+1];
-            int[] indeg = new int[n+1];
-            ArrayList<Integer>[] adj = new ArrayList[n+1];
+            arr = new int[n + 1];
+            arr_done = new int[n + 1];
+            indeg = new int[n + 1];
+            al = new ArrayList[n + 1];
 
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= n; i++) {
-                adj[i] = new ArrayList();
+                al[i] = new ArrayList();
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int i = 1; i <= k; i++) {
+            for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-
-                adj[x].add(y);
-                // indegree 계산하기
+                al[x].add(y);
                 indeg[y]++;
             }
-            // 도착지점
-            int end = Integer.parseInt(br.readLine());
+            w = Integer.parseInt(br.readLine());
 
-            Queue<Integer> q = new LinkedList();
-            // 제일 앞에 "정렬될 수 있는" 정점 찾기
-            for (int i = 1; i <= n; i++) {
-                if (indeg[i] == 0) {
-                    q.add(i);
-                    arr_done[i] = arr[i];
-                }
-            }
-            // 위상 정렬 순서대로 T_done 계산을 함께 해주기
-            while(!q.isEmpty()) {
-                int x = q.poll();
-                for (int y : adj[x]) {
-                    indeg[y]--;
-                    if (indeg[y] == 0) q.add(y);
-                    // arr_done에 해당 거리의 최대값 갱신
-                    arr_done[y] = Math.max(arr_done[y], arr_done[x] + arr[y]);
-                }
-            }
+            bfs();
 
-            System.out.println(arr_done[end]);
+            System.out.println(arr_done[w]);
         }
-        
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList();
+
+        for (int i = 1; i <= n; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+                arr_done[i] = arr[i];
+            }
+        }
+
+        while(!q.isEmpty()) {
+            int x = q.poll();
+
+            for (int y : al[x]) {
+                arr_done[y] = Math.max(arr_done[y], arr_done[x] + arr[y]);
+                indeg[y]--;
+                if (indeg[y] == 0) q.add(y);
+            }
+        }
     }
 }

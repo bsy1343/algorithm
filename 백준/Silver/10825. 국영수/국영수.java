2127ad50d7a2fcf7b7af32c8092439b44b2ad039
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-07-29 17:35:02 +0900

    [Silver IV] Title: 국영수, Time: 1140 ms, Memory: 65092 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,9 +1,8 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static class Node implements Comparable<Node>{
+public class Main{
+    static class Node implements Comparable<Node> {
         String name;
         int k, e, m;
 
@@ -16,25 +15,18 @@ public class Main {
 
         @Override
         public int compareTo(Node node) {
-            // 국어 점수 내림차순
             if (this.k != node.k) return node.k - this.k;
-
-            // 영어 점수 오름차순
             if (this.e != node.e) return this.e - node.e;
-
-            // 수학 점수 내림차순
             if (this.m != node.m) return node.m - this.m;
 
-            // 이름 오름차순
             return this.name.compareTo(node.name);
         }
-
     }
 
     static int n;
     static ArrayList<Node> al = new ArrayList();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -42,10 +34,12 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
+
             String name = st.nextToken();
             int k = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
+
             al.add(new Node(name, k, e, m));
         }
 
@@ -54,6 +48,5 @@ public class Main {
         for (Node node : al) {
             System.out.println(node.name);
         }
-
     }
 }

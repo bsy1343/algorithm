ea0cf70b327f788587a6598dc9e34ff080c96231
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-08-07 13:21:13 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 21012 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int n, m, v;
     static int[] visited;
-    static int[][] map;
     static ArrayList<Integer>[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -14,21 +13,19 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
-        map = new int[n+1][n+1];
-        visited = new int[n+1];
-        arr = new ArrayList[n+1];
 
+        visited = new int[n + 1];
+        arr = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             arr[i] = new ArrayList();
         }
 
-        for (int i = 1; i <= m; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            map[x][y] = 1;
-            map[y][x] = 1;
 
+            // 양방향
             arr[x].add(y);
             arr[y].add(x);
         }
@@ -38,23 +35,18 @@ public class Main {
         }
 
         dfs(v);
+
         System.out.println();
+        // 초기화
         Arrays.fill(visited, 0);
+
         bfs(v);
     }
 
     static void dfs(int x) {
         visited[x] = 1;
-
         System.out.print(x + " ");
-        // todo: 인접행렬
-        /*for (int y = 1; y <= n; y++) {
-            if (visited[y] == 1) continue;
-            if (map[x][y] == 0) continue;
-            dfs(y);
-        }*/
 
-        // todo: 인접리스트
         for (int y : arr[x]) {
             if (visited[y] == 1) continue;
             dfs(y);
@@ -63,27 +55,18 @@ public class Main {
 
     static void bfs(int x) {
         Queue<Integer> q = new LinkedList();
-        visited[x] = 1;
         q.add(x);
-        while (!q.isEmpty()) {
+        visited[x] = 1;
+
+        while(!q.isEmpty()) {
             x = q.poll();
             System.out.print(x + " ");
 
-            // todo: 인접행렬
-            /*for (int y = 1; y <= n; y++) {
-                if (visited[y] == 1) continue;
-                if (map[x][y] == 0) continue;
-                q.add(y);
-                visited[y] = 1;
-            }*/
-
-            // todo: 인접리스트
             for (int y : arr[x]) {
                 if (visited[y] == 1) continue;
-                q.add(y);
                 visited[y] = 1;
+                q.add(y);
             }
         }
-
     }
 }

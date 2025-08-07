27bcbe131b7ef423e0b80aed37f8656a86f25448
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-08-07 11:34:07 +0900

    [Gold IV] Title: 고냥이, Time: 260 ms, Memory: 20716 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,52 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int n, kind;
+    static int n, kind, answer;
     static int[] visited;
+    static char[] arr;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        kind = 0;
         visited = new int[26];
-        String str = sc.next();
-        char[] arr = str.toCharArray();
 
-        int len = arr.length, answer = 0;
-        for (int l = 0, r = 0; r < len; r++) {
-            // r번째 문자를 오른쪽에 추가
+        String str = sc.next();
+        arr = str.toCharArray();
+
+        int l = 0;
+        for (int r = 0; r < arr.length; r++) {
             add(arr[r]);
 
-            // 불가능하면, 가능할때까지 l을 이동
             while(kind > n) {
-                erase(arr[l++]);
+                remove(arr[l++]);
             }
 
-            // 정답갱신
             answer = Math.max(answer, r - l + 1);
         }
 
         System.out.println(answer);
     }
 
-    // x라는 알파벳 추가
-    static void add(char x) {
-        visited[x-'a']++;
-
-        // 새롭게 나타난 알파벳이라는 뜻
-        if (visited[x-'a'] == 1) {
-            kind++;
-        }
+    static void add(int x) {
+        visited[x - 'a']++;
+        if (visited[x - 'a'] == 1) kind++;
     }
 
-    // x라는 알파벳 제거
-    static void erase(char x) {
-        visited[x-'a']--;
-
-        // 인식해야 하는 알파벳에서 빠지는 순간
-        if (visited[x-'a'] == 0) {
-            kind--;
-        }
+    static void remove(int x) {
+        visited[x - 'a']--;
+        if (visited[x - 'a'] == 0) kind--;
     }
 }

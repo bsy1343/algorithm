46fd7b41c139ccdfe077f8cd4b68da3e3abfe9eb
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-11 01:20:50 +0900

    [level 2] Title: H-Index, Time: 1.73 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,17 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(citations);
         
+        Arrays.sort(citations);
         for (int i = 0; i < citations.length; i++) {
             int h = citations.length - i;
-            
             if (citations[i] >= h) {
                 answer = h;
                 break;
             }
-            
         }
         return answer;
     }

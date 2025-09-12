31a3da47928731ee898a31463f131c366bb421d9
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-12 13:25:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.99 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,60 +2,52 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    
     static class Node {
-        int x;
-        int y;
-        int s;
-        Node (int x, int y, int s) {
+        int x, y, dist;
+        Node (int x, int y, int dist) {
             this.x = x;
             this.y = y;
-            this.s = s;
+            this.dist = dist;
         }
     }
-    
-    static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     static int n, m, answer;
-    static int[][] maps, visited;
-        
+    static int[][] visited, maps;
+    static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
     public int solution(int[][] maps) {
-        this.answer = -1;
-        this.maps = maps;
-        this.n = maps.length;
-        this.m = maps[0].length;
-        this.visited = new int[n][m];
+        Solution.maps = maps;
+        n = maps.length;
+        m = maps[0].length;
+        visited = new int[n][m];
         
-        bfs(0, 0, maps[0][0]);
+        bfs(0, 0, 1);
         
-        return answer;
+        return answer == 0 ? -1 : answer;
     }
     
-    
-    private void bfs(int x, int y, int s) {
+    static void bfs(int x, int y, int dist) {
         Queue<Node> q = new LinkedList();
+        q.add(new Node(x, y, dist));
         visited[x][y] = 1;
-        q.add(new Node(x, y, s));
         
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-            
-            if (now.x == n-1 && now.y == m-1) {
-                answer = now.s;
+        while(!q.isEmpty()) {
+            Node node  = q.poll();
+
+            if (node.x == n-1 && node.y == m-1) {
+                answer = node.dist;
                 return;
             }
             
-            for (int i = 0; i < direction.length; i++) {
-                int dx = direction[i][0] + now.x;
-                int dy = direction[i][1] + now.y;
+            for (int i = 0; i < directions.length; i++) {
+                int nx = node.x + directions[i][0];
+                int ny = node.y + directions[i][1];
                 
-                if (dx < 0 || dy < 0 || dx >= n || dy >= m || maps[dx][dy] == 0 ||visited[dx][dy] == 1) {
-                    continue;
-                }
-                
-                visited[dx][dy] = 1;
-                q.add(new Node(dx, dy, now.s+1));
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (visited[nx][ny] != 0) continue;
+                if (maps[nx][ny] != 1) continue;
+                visited[nx][ny] = 1;
+                q.add(new Node(nx, ny, node.dist + 1));
             }
-            
         }
     }
 }

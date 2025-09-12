207744e3152c853a0ee23dba470a6fe153206c70
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-12 13:04:14 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,17 +1,19 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     static int n, answer;
+    static int[] visited;
     static int[][] computers;
-    static int visit[];
     
     public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.computers = computers;
-        this.visit = new int[n];
-        this.answer = 0;
+        Solution.n = n;
+        Solution.computers = computers;
+        visited = new int[n];
         
         for (int i = 0; i < n; i++) {
-            if (visit[i] == 0) {
-                answer++;   
+            if(visited[i] == 0) {
+                answer++;
                 dfs(i);
             }
         }
@@ -19,11 +21,12 @@ class Solution {
         return answer;
     }
     
-    private void dfs(int i ) {
-        visit[i] = 1;
-        for (int j = 0; j < n; j++) {
-            if (computers[i][j] == 1 && visit[j] == 0) {
-                dfs(j);
+    // row를 돌면서, 열에서 1이면서 아직 방문 안 한 곳 → 같은 네트워크로 이어지고, 그렇게 연결된 덩어리 하나가 네트워크.
+    static void dfs(int row) {
+        visited[row] = 1;
+        for (int col = 0; col < n; col++) {
+            if (visited[col] == 0 && computers[row][col] == 1) {
+                dfs(col);
             }
         }
     }

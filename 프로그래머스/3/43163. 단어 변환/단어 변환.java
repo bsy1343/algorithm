5947b006c46b5460079f4a1fed510ae6dab33d17
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-12 14:14:00 +0900

    [level 3] Title: 단어 변환, Time: 1.20 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,70 @@
 import java.util.*;
-import java.io.*;
 
-// https://jisunshine.tistory.com/157 참고
 class Solution {
-    static int answer;
-    static int[] visit;
-    public int solution(String begin, String target, String[] words) {
-        answer = 0;
-        visit = new int[words.length];
-        
-        dfs(begin, target, words, 0);
-        return answer;
+    static class Node {
+        String word;
+        int cnt;
+        Node(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
     }
     
-    public static void dfs(String begin, String target, String[] words, int cnt) {
-        if (begin.equals(target)) {
-            answer = cnt;
-            return;
-        }
+    static int n, answer;
+    static int[] visited;
+    static String[] words;
+    static String begin, target;
+
+    public int solution(String begin, String target, String[] words) {
+        Solution.begin = begin;
+        Solution.target = target;
+        Solution.n = words.length;
+        Solution.words = words;
+        visited = new int[n];
+
+        // target이 아예 words 안에 없으면 불가능
+        // boolean exist = false;
+        // for (String w : words) {
+        //     if (w.equals(target)) exist = true;
+        // }
+        // if (!exist) return 0;
         
-        for (int i = 0; i < words.length; i++) {
-            if (visit[i] == 1) {
-                continue;
+        bfs();
+
+        return answer; // 못 도달했을 때
+    }
+    
+    static void bfs() {
+         // 큐: [현재단어, 단계수]
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(begin, 0));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            // 목표 도착하면 그때의 단계수 반환 (최단 거리 보장됨)
+            if (cur.word.equals(target)) {
+                answer = cur.cnt;
+                return;
             }
-            
-            int k = 0;
-            for (int j = 0; j < begin.length(); j++) {
-                if (begin.charAt(j) == words[i].charAt(j)) {
-                    k++;
+
+            // 모든 단어를 돌면서 "한 글자만 다른 단어"면 큐에 추가
+            for (int i = 0; i < n; i++) {
+                if (visited[i] == 1) continue;
+                if (diff1(cur.word, words[i])) {
+                    visited[i] = 1;
+                    q.add(new Node(words[i], cur.cnt + 1));
                 }
-            }
-            
-            if (k == begin.length() -1) {
-                visit[i] = 1;
-                System.out.println("dfs : i="+ i +" : "+words[i] + " : " + target + " : " + (cnt+1) );
-                dfs(words[i], target, words, cnt+1);
-                System.out.println("백트래킹 : i="+ i +" : "+words[i] + " : " + target + " : " + (cnt+1) );
-                visit[i] = 0;
             }
         }
     }
+
+    static boolean diff1(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) diff++;
+        }
+        return diff == 1; // 정확히 한 글자만 다르면 true
+    }
+    
 }

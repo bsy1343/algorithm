8ca738dad7e7d08702b936e2dec9ed5d0719e42a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-13 03:58:57 +0900

    [level 3] Title: 여행경로, Time: 77.81 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,18 +2,19 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    
     static String[][] tickets;
     
+    static int answer;
     static int[] visited;
     static ArrayList<String> al = new ArrayList();
     
     public String[] solution(String[][] tickets) {
-        
         Solution.tickets = tickets;
+        answer = 0;
         visited = new int[tickets.length];
         
         dfs(0, "ICN", "ICN");
+        
         Collections.sort(al);
         
         return al.get(0).split(" ");
@@ -22,15 +23,13 @@ class Solution {
     static void dfs(int idx, String now, String result) {
         if (idx == tickets.length) {
             al.add(result);
-            return;
         } else {
             for (int i = 0; i < tickets.length; i++) {
-                if (visited[i] != 0) continue;
+                if (visited[i] != 0) continue; 
                 if (!now.equals(tickets[i][0])) continue;
                 visited[i] = 1;
                 dfs(idx + 1, tickets[i][1], result + " " + tickets[i][1]);
                 visited[i] = 0;
-                
             }
         }
     }

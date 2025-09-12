cb874b8db25f60955aff116adb2f5ab4002776aa
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-12 16:33:33 +0900

    [level 3] Title: 여행경로, Time: 186.23 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,35 +2,35 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[] visit;
-    static ArrayList<String> al;
+    
+    static String[][] tickets;
+    
+    static int[] visited;
+    static ArrayList<String> al = new ArrayList();
     
     public String[] solution(String[][] tickets) {
-        visit = new int[tickets.length];
-        al = new ArrayList();
         
-        // dfs를 통해 모든것 돌리기
-        dfs(0, "ICN", "ICN", tickets);
+        Solution.tickets = tickets;
+        visited = new int[tickets.length];
         
-        
-        // 답안 제출 시 알파벳 순으로 정렬
+        dfs(0, "ICN", "ICN");
         Collections.sort(al);
         
         return al.get(0).split(" ");
     }
     
-    private static void dfs(int depth, String start, String path, String[][] map) {
-        if (depth == map.length) {
-            al.add(path);
-            // System.out.println(path);
+    static void dfs(int idx, String now, String result) {
+        if (idx == tickets.length) {
+            al.add(result);
             return;
-        }
-        
-        for (int i = 0; i < map.length; i++) {
-            if (visit[i] == 0 && start.equals(map[i][0])) {
-                visit[i] = 1;
-                dfs(depth + 1, map[i][1], path + " " + map[i][1], map);
-                visit[i] = 0;
+        } else {
+            for (int i = 0; i < tickets.length; i++) {
+                if (visited[i] != 0) continue;
+                if (!now.equals(tickets[i][0])) continue;
+                visited[i] = 1;
+                dfs(idx + 1, tickets[i][1], result + " " + tickets[i][1]);
+                visited[i] = 0;
+                
             }
         }
     }

5f2a8789d0a9b81417ef5d55512e22789fbdfbfa
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-13 03:17:58 +0900

    [level 3] Title: 아이템 줍기, Time: 1.34 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,82 +1,108 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
+
     static class Node {
-        int x, y, s;
-        Node (int x, int y, int s) {
+        int x, y, dist;
+        Node (int x, int y, int dist) {
             this.x = x;
             this.y = y;
-            this.s = s;
+            this.dist = dist;
         }
     }
+    
+    static int[][] rectangle;
+    static int characterX, characterY, itemX, itemY;
+    
     static int answer;
-    static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    static int[][] map;
-    static int[][] visit;
+    static int[][] visited, map;
+    static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        // 정의를 받는다.
-        map = new int[102][102];
-        visit = new int[102][102];
+        Solution.rectangle = rectangle;
+        Solution.characterX = characterX;
+        Solution.characterY = characterY;
+        Solution.itemX = itemX;
+        Solution.itemY = itemY;
+        
         answer = 0;
+        visited = new int[101][101];
+        map = new int[101][101];
         
-        Scanner sc = new Scanner(System.in);
-        int sizeX = rectangle.length;
-        int sizeY = rectangle[0].length;
-        
-        // 모든 네모를 1로 채운다
-        for (int t = 0; t < sizeX; t++) {
-            int fx = rectangle[t][0] * 2;
-            int fy = rectangle[t][1] * 2;
-            int sx = rectangle[t][2] * 2;
-            int sy = rectangle[t][3] * 2;
+        for (int i = 0; i < rectangle.length; i++) {
+            int ax = rectangle[i][0] * 2;
+            int ay = rectangle[i][1] * 2;
+            int bx = rectangle[i][2] * 2;
+            int by = rectangle[i][3] * 2;
             
-            for (int i = fx; i <= sx; i++) {
-                for (int j = fy; j <= sy; j++) {
-                    map[i][j] = 1;
+            for (int x = ax; x <= bx; x++) {
+                for (int y = ay; y <= by; y++) {
+                    map[x][y] = 1;
                 }
             }
         }
         
-        // 네모 안을 0으로 만든다
-        for (int t = 0; t < sizeX; t++) {
-            int fx = rectangle[t][0] * 2;
-            int fy = rectangle[t][1] * 2;
-            int sx = rectangle[t][2] * 2;
-            int sy = rectangle[t][3] * 2;
+        for (int i = 0; i < rectangle.length; i++) {
+            int ax = rectangle[i][0] * 2;
+            int ay = rectangle[i][1] * 2;
+            int bx = rectangle[i][2] * 2;
+            int by = rectangle[i][3] * 2;
             
-            for (int i = fx+1; i < sx; i++) {
-                for (int j = fy+1; j < sy; j++) {
-                    map[i][j] = 0;
+            for (int x = ax + 1; x < bx; x++) {
+                for (int y = ay + 1; y < by; y++) {
+                    map[x][y] = 0;
                 }
             }
         }
         
-        // bfs를 통해 값을 도출해낸댜.
-        Queue<Node> q = new LinkedList();
-        q.add(new Node(characterX*2, characterY*2, 0));
-        visit[characterX*2][characterY*2] = 1;
-        
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-            
-            if (now.x == itemX*2 && now.y == itemY*2) {
-                answer = now.s/2;
-                break;
-            }
-            
-            for (int i = 0; i < direction.length; i++) {
-                int nx = now.x + direction[i][0];
-                int ny = now.y + direction[i][1];
-                
-                if ( nx > -1 && ny > -1 && nx <= 102 && ny <= 102 && visit[nx][ny] == 0 && map[nx][ny] == 1) {
-                    visit[nx][ny] = 1;
-                    q.add(new Node(nx, ny, now.s+1));
-                }
-            }
-        }
+        bfs(characterX * 2, characterY * 2, 0);
         
         return answer;
     }
+    
+    static void bfs(int x, int y, int dist) {
+        Queue<Node> q = new LinkedList();
+        q.add(new Node(x, y, dist));
+        visited[x][y] = 1;
+        
+        while(!q.isEmpty()) {
+            Node node = q.poll();
+            
+            if (node.x == itemX * 2 && node.y == itemY * 2) {
+                answer = node.dist/2;
+                return;
+            }
+            
+            for (int i = 0; i < directions.length; i++) {
+                int dx = node.x + directions[i][0];
+                int dy = node.y + directions[i][1];
+                
+                if (dx < 0 || dy < 0 || dx >= 101 || dy >= 101) continue;
+                if (visited[dx][dy] != 0) continue;
+                if (map[dx][dy] != 1) continue;
+                
+                visited[dx][dy] = 1;
+                q.add(new Node(dx, dy, node.dist + 1));
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

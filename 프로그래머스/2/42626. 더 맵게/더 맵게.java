27c4a3a9f815c8e464e1e3324596a4a0b4da6e72
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-14 05:33:52 +0900

    [level 2] Title: 더 맵게, Time: 1585.48 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,19 +1,22 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
+    
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소힙
-        for (int s : scoville) pq.add(s);
-
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for (int i : scoville) pq.add(i);
+        
         int count = 0;
-        // 최소값이 K 미만이고, 섞을 재료(최소 2개)가 있을 때만 진행
-        while (pq.size() > 1 && pq.peek() < K) {
-            int a = pq.poll();      // 최소
-            int b = pq.poll();      // 두 번째 최소
-            pq.add(a + 2 * b);      // 새 음식
+        while(pq.size() > 1 && pq.peek() < K ) {
+            int a = pq.poll();
+            int b = pq.poll();
+            
+            pq.add(a + b * 2);
+            
             count++;
         }
-        // 모두 K 이상이면 성공, 아니면 실패(-1)
+        
         return pq.peek() >= K ? count : -1;
     }
 }

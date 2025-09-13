3ff9890eb0724a120958dde348f8a19a6a6046ac
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-13 21:51:20 +0900

    [level 2] Title: 주식가격, Time: 14.99 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = count(i, prices);
+        }
+        
+        return answer;
+    }
+    
+    static int count(int idx, int[] prices) {
+        int cnt = 0;
+        for (int i = idx+1; i < prices.length; i++) {
+            cnt++;
+            if (prices[i] < prices[idx]) break;
+        }
+        return cnt;
+    }
+}

1b3330ec38d6a17a5fe1fffae3151801a31ac299
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-14 22:12:59 +0900

    [level 2] Title: 구명보트, Time: 11.29 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,11 +4,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
-        
+
         int answer = 0;
         int l = 0;
         int r = people.length - 1;
-        
+
         // 가장 무거운 사람은 무조건 태워야 함
         // 혼자 타거나, 가장 가벼운 사람과 같이 타거나 2가지뿐
         // 둘이 못 타면 무거운 사람 혼자, 둘이 타면 둘 다
@@ -19,7 +19,7 @@ class Solution {
             r--;
             answer++;
         }
-        
+
         return answer;
     }
 }

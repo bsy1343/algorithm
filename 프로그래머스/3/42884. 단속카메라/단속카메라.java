009dc81b7fad66242081676e060d716c722e0012
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-14 22:55:27 +0900

    [level 3] Title: 단속카메라, Time: 17.38 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,6 +1,20 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, Comparator.comparingInt(o -> o[1]));
         int answer = 0;
+        int camera = Integer.MIN_VALUE;
+        for (int[] route : routes) {
+            int start = route[0];
+            int end = route[1];
+            
+            if (camera < start) {
+                answer++;
+                camera = end;
+            }
+        }
         return answer;
     }
 }

98036af8bcb4a56b6a45b574d25ff3cb0649f57a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-15 23:02:17 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67880 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -6,28 +6,29 @@ public class Main {
     static int n, m;
     static int[] selected;
     static StringBuilder sb = new StringBuilder();
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        selected = new int[m + 1];
-
-        dfs(1);
-
-        System.out.println(sb);
+        
+        selected = new int[n];
+        
+        dfs(0);
+        
+        System.out.println(sb.toString());
     }
-
+    
     static void dfs(int idx) {
-        if (idx > m) {
-            for (int i = 1; i <= m; i++) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
                 sb.append(selected[i]).append(" ");
             }
             sb.append("\n");
         } else {
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n; i++ ) {
                 selected[idx] = i;
                 dfs(idx + 1);
             }

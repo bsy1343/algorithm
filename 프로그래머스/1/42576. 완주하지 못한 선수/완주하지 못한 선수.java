30c2feb6cf0ddbebcf8914c3ebc8951bb6f13511
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-17 22:49:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.15 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,25 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap();
+        
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+        
         String answer = "";
-        
-        HashMap<String, Integer> = map = new HashMap();
-       
-        for (int i = 0; i < participant.length, i++) {
-            map.put(participant[i], 0);
-        }
-        
-        for (int i = 0; i < completion.length, i++) {
-            map.put(completion[i], 1);
-        }
-        
-        for (Map.Entry(String, Integer) entry : map.entrySet()) {
-            if (entry.getValue() == 0) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
                 answer = entry.getKey();
-            }
+                break;
+            } 
         }
-        
         
         return answer;
     }

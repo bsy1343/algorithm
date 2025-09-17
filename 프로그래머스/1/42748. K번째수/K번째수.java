3b2c9e16b43ecdb40453a0e77619c05449f3b23f
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 00:24:56 +0900

    [level 1] Title: K번째수, Time: 2.76 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
         ArrayList<Integer> result = new ArrayList();
         
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int idx = commands[i][2];
-            
             ArrayList<Integer> al = new ArrayList();
-            for (int j = start-1; j < end; j++) {
+            
+            int x = commands[i][0];
+            int y = commands[i][1];
+            int z = commands[i][2];
+            
+            for (int j = x-1; j < y; j++) {
                 al.add(array[j]);
             }
+            
             Collections.sort(al);
-            result.add(al.get(idx-1));
+            
+            result.add(al.get(z-1));
         }
-        answer = result.stream().mapToInt(o -> o).toArray();
-        return answer;
+        
+        return result.stream().mapToInt(o -> o).toArray();
     }
 }

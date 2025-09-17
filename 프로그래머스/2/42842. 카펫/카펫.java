a0f1233cf852efe37d8262fa4dba028983ec3e32
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 02:29:49 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,27 +1,23 @@
 import java.io.*;
 import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
         int total = brown + yellow;
         
-        // 세로부터 시작해서 가로까지 탐색 (가로 >= 세로 조건)
-        for (int h = 3; h <= Math.sqrt(total); h++) {
-            if (total % h == 0) {
-                int w = total / h;
-                
-                // 노란색 격자 개수 확인
-                int yellowCount = (w - 2) * (h - 2);
-                
-                if (yellowCount == yellow) {
-                    answer[0] = w;  // 가로
-                    answer[1] = h;  // 세로
-                    break;
-                }
+        for (int h = 3; h <= total; h++) {
+            if (total % h != 0) continue;
+            int w = total / h;
+            
+            int yellowCount = (h-2) * (w-2);
+            
+            if (yellowCount == yellow) {
+                answer[0] = w;
+                answer[1] = h;
+                break;
             }
         }
-        
         return answer;
     }
 }

de6aa3bd3ca091da94e348510723183639533b4d
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 01:41:05 +0900

    [level 1] Title: 모의고사, Time: 3.90 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -12,19 +12,17 @@ class Solution {
         int[] result = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == humans[0][i%humans[0].length]) result[0]++;
-            if (answers[i] == humans[1][i%humans[1].length]) result[1]++;
-            if (answers[i] == humans[2][i%humans[2].length]) result[2]++;
+            if (answers[i] == humans[0][i % humans[0].length]) result[0]++;
+            if (answers[i] == humans[1][i % humans[1].length]) result[1]++;
+            if (answers[i] == humans[2][i % humans[2].length]) result[2]++;
         }
         
         int max = Math.max(result[0], Math.max(result[1], result[2]));
-        ArrayList<Integer> al = new ArrayList();
         
-        for (int i = 0; i < result.length; i++) {
+        ArrayList<Integer> al = new ArrayList();
+        for (int i = 0; i < 3; i++) {
             if (result[i] == max) al.add(i+1);
         }
-        
-        answer = al.stream().mapToInt(o -> o).toArray();
-        return answer;
+        return al.stream().mapToInt(o -> o).toArray();
     }
 }

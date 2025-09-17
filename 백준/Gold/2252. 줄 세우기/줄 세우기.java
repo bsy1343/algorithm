360729e21b85c175e047dc11c13f66b194ffdaed
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-17 12:18:00 +0900

    [Gold III] Title: 줄 세우기, Time: 396 ms, Memory: 46252 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,48 +3,55 @@ import java.util.*;
 
 public class Main {
 
+    static int n, m;
+    static int[] indeg;
+    static ArrayList<Integer>[] al;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] indeg = new int[n+1];
-        ArrayList<Integer>[] adj = new ArrayList[n+1];
-        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) adj[i] = new ArrayList();
+        indeg = new int[n + 1];
+        al = new ArrayList[n + 1];
 
-        for (int i = 1; i <=m; i++) {
+        for (int i = 1; i <= n; i++) {
+            al[i] = new ArrayList();
+        }
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
-            adj[x].add(y);
-            // indegree 계산하기
+            al[x].add(y);
             indeg[y]++;
         }
 
+        bfs();
+
+        System.out.println(sb.toString());
+    }
+
+    static void bfs() {
         Queue<Integer> q = new LinkedList();
-        // 제일 앞에 "정렬될 수 있는" 정점 찾기
+
         for (int i = 1; i <= n; i++) {
             if (indeg[i] == 0) q.add(i);
         }
 
-        // 정렬될 수 있는 정점이 있다면?
-        // 1. 정렬 결과에 추가하기
-        // 2. 정점과 연결된 간선 제거하기
-        // 3. 새롭게 "정렬 될 수 있는" 정점 Queue에 추가하기
         while(!q.isEmpty()) {
             int x = q.poll();
+
             sb.append(x).append(" ");
 
-            for (int y : adj[x]) {
+            for (int y : al[x]) {
                 indeg[y]--;
                 if (indeg[y] == 0) q.add(y);
             }
         }
 
-        System.out.println(sb);
     }
 }

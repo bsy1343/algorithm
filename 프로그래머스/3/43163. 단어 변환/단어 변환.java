66bc08d96200ef1e42c22bf5eb9214790dccb6fa
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 12:51:47 +0900

    [level 3] Title: 단어 변환, Time: 0.68 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,72 +1,59 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     static class Node {
-        String name;
+        String str;
         int cnt;
-        Node (String name, int cnt) {
-            this.name = name;
+        Node (String str, int cnt) {
+            this.str = str;
             this.cnt = cnt;
         }
     }
-    
     static String begin, target;
     static String[] words;
     
-    static int answer;
     static int[] visited;
-    
+    static int answer;
     public int solution(String begin, String target, String[] words) {
         Solution.begin = begin;
         Solution.target = target;
         Solution.words = words;
-        visited = new int[words.length];
-        answer = 0;
         
-        bfs(begin, 0);
+        visited = new int[words.length];
+        
+        bfs(begin);
         
         return answer;
     }
     
-    static void bfs(String name, int cnt) {
+    static void bfs(String s) {
         Queue<Node> q = new LinkedList();
-        q.add(new Node(name, cnt));
+        q.add(new Node(s, 0));
         
         while(!q.isEmpty()) {
             Node node = q.poll();
             
-            if (node.name.equals(target)) {
+            if (node.str.equals(target)) {
                 answer = node.cnt;
-                return;
+                break;
             }
             
             for (int i = 0; i < words.length; i++) {
                 if (visited[i] != 0) continue;
-                if (diff(node.name, words[i])) {
-                    visited[i] = 1;
-                    q.add(new Node(words[i], node.cnt + 1));
-                }
+                if (!isCheck(node.str, words[i])) continue;
+                visited[i] = 1;
+                q.add(new Node(words[i], node.cnt + 1));
             }
         }
     }
     
-    static boolean diff(String a, String b) {
-        int count = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) count++;
+    static boolean isCheck(String a, String b) {
+        int length = a.length();
+        int cnt = 0;
+        for (int i = 0; i < length; i++) {
+            if (a.charAt(i) != b.charAt(i)) cnt++;
         }
-        return count == 1;
+        return cnt == 1;
     }
 }
-
-
-
-
-
-
-
-
-
-
-

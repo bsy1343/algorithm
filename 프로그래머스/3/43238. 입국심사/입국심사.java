accef0ec153dc7e205a90f9b292453440f95b48f
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-19 03:12:38 +0900

    [level 3] Title: 입국심사, Time: 42.54 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,40 +1,32 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-
-        long minTime = 1;
-        long maxTime = (long) times[times.length - 1] * n; // 최악의 시간
-        long answer = maxTime;  // 가능한 값 중 최소를 찾음
-
-        while (minTime <= maxTime) {
-            // 기준 시간을 검색 (이분탐색)
-            long mid = (minTime + maxTime) / 2;
-
-            // 현재 시간에 심사 가능한 사람 수 체크
-            if (canFinishInTime(mid, n, times)) {
-                answer = mid;       // 가능하면 답 업데이트
-                maxTime = mid - 1;  // 더 짧은 시간 시도
+        
+        long answer = 0;
+        long min = 1;
+        long max = (long) times[times.length - 1] * n;
+        
+        while(min <= max) {
+            long mid = (min + max) / 2;
+            
+            if (isCheck(mid, n, times)) {
+                answer = mid;
+                max = mid - 1;
             } else {
-                minTime = mid + 1;  // 시간 늘리기
+                min = mid + 1;
             }
         }
-
         return answer;
     }
-
-    private boolean canFinishInTime(long curTime, int n, int[] times) {
-        long totalPeople = 0;
-
-        // curTime 시간 동안 처리 가능한 총 인원
-        for (int t : times) {
-            totalPeople += curTime / t;
-
-            if (totalPeople >= n) return true; // 이미 충분
+    
+    static boolean isCheck(long curTime, int n, int[] times) {
+        long total = 0;
+        
+        for (int time : times) {
+            total += curTime/time;
+            if (total >= n) return true;
         }
-
         return false;
     }
 }

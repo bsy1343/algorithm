7f861158ab324d7b6c50e473314deddb42a4d8b7
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-19 03:28:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 35.49 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,43 +1,32 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int n;
-    static int[][] edge;
-    
     static int[] visited;
     static ArrayList<Integer>[] al;
+    static int max;
+    
     public int solution(int n, int[][] edge) {
-        Solution.n = n;
-        Solution.edge = edge;
         
         visited = new int[n + 1];
         al = new ArrayList[n + 1];
+        
+        Arrays.fill(visited, -1);
         
         for (int i = 1; i <= n; i++) {
             al[i] = new ArrayList();
         }
         
-        Arrays.fill(visited, -1);
-        
-        for (int i = 0; i < edge.length; i++) {
-            al[edge[i][0]].add(edge[i][1]);
-            al[edge[i][1]].add(edge[i][0]);
+        for (int[] x : edge) {
+            al[x[0]].add(x[1]);
+            al[x[1]].add(x[0]);
         }
         
         bfs(1);
-        
-        
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, visited[i]);
-        }
         
         int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (visited[i] == max) answer++;
         }
-        
         return answer;
     }
     
@@ -50,11 +39,11 @@ class Solution {
             int x = q.poll();
             
             for (int y : al[x]) {
-                if (visited[y] != -1 ) continue;
+                if (visited[y] != -1) continue;
                 visited[y] = visited[x] + 1;
+                max = Math.max(max, visited[y]);
                 q.add(y);
             }
-            
         }
     }
 }

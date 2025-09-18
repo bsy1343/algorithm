8e5929da937d8ba9c6be304633d2a028b33184f6
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-19 01:13:37 +0900

    [level 1] Title: 체육복, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,36 +1,30 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] visited = new int[n + 1];
+        int[] arr = new int[n + 1];
         
-        Arrays.fill(visited, 1);
+        Arrays.fill(arr, 1);
         
-        for (int i : lost) visited[i]--;
-        for (int i : reserve) visited[i]++;
+        for (int x : lost) arr[x]--;
+        for (int x : reserve) arr[x]++;
         
-        int cnt = lost.length;
         for (int i = 1; i <= n; i++) {
-            if (visited[i] == 0) {
-                if (i > 1 && visited[i - 1] == 2) {
-                    visited[i-1]--;
-                    visited[i]++;
-                    cnt--;
-                } else if (i < n && visited[i + 1] == 2) {
-                    visited[i+1]--;
-                    visited[i]++;
-                    cnt--;
-                }
+            if (arr[i] != 0) continue;
+            if (i > 1 && arr[i - 1] == 2) {
+                arr[i]++;
+                arr[i - 1]--;
+            } else if (i < n && arr[i + 1] == 2) {
+                arr[i]++;
+                arr[i + 1]--;
             }
         }
+        
         int answer = 0;
-        for (int i = 1; i <= n ; i++) {
-            if (visited[i] >= 1) answer++;
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] >= 1) answer++;
         }
         
-        System.out.println(answer);
-        System.out.println((n-cnt));
         return answer;
     }
 }

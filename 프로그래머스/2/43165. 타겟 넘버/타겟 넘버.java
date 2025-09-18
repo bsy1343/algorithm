9f3b68687fdb3f9fa1f413fc4e8b94e32bd3b772
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 11:10:31 +0900

    [level 2] Title: 타겟 넘버, Time: 8.97 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     static int[] numbers;
-    static int target, answer;
-    
+    static int target;
+    static int answer;
     public int solution(int[] numbers, int target) {
-        
-        this.numbers = numbers;
-        this.target = target;
+        Solution.numbers = numbers;
+        Solution.target = target;
+        answer = answer;
         
         dfs(0, 0);
         
         return answer;
     }
     
-    static void dfs (int idx, int sum) {
+    static void dfs(int idx, int result) {
         if (idx == numbers.length) {
-            if (sum == target) answer++; 
+            if (result == target) {
+                answer++;
+            }
         } else {
-            dfs(idx + 1, sum + numbers[idx]);
-            dfs(idx + 1, sum - numbers[idx]);
+            dfs(idx + 1, result + numbers[idx]);
+            dfs(idx + 1, result - numbers[idx]);
         }
     }
 }

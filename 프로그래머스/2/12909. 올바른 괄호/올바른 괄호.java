659a308340f50db982588ef2de61acc08a19d689
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-18 17:25:12 +0900

    [level 2] Title: 올바른 괄호, Time: 6.78 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,15 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
+
         char[] arr = s.toCharArray();
-        
         int cnt = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (cnt == 0 && arr[i] == ')') return false;
-            
-            if (arr[i] == '(') cnt++;
-            if (arr[i] == ')') cnt--;
+        for (char c : arr) {
+            if (cnt == 0 && c == ')') return false;
+            if (c == '(') cnt++;
+            if (c == ')') cnt--;
         }
 
         return cnt == 0;

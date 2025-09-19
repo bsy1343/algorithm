1960f761c81915fbc06131a7e47072f5704677f4
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2025-09-20 02:30:20 +0900

    [level 2] Title: 기능개발, Time: 2.78 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -14,6 +14,7 @@ class Solution {
             int cnt = 0;
             for (int i = 0; i < n; i++) {
                 if (visited[i] != 0) continue;
+                // 방문을 하지 않았을떄 조건이 안맞으면 break 시킴 (두번째 프로세스가 완료되어도 첫번쨰가 완료 안되면 break조건)
                 if (progresses[i] + day * speeds[i] >= 100) {
                     visited[i] = 1;
                     cnt++;

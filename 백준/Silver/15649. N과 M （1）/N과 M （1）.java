c218986d5afb66a25bfbc5420d7004de02cc4b44
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-02-01 07:06:19 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 21884 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
@@ -7,30 +7,30 @@ public class Main {
     static int[] selected, visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        selected = new int[n + 1];
+        selected = new int[m];
         visited = new int[n + 1];
 
-        dfs(1);
+        dfs(0);
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     static void dfs(int idx) {
-        if (idx > m) {
-            for (int i = 1; i <= m; i++) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
                 sb.append(selected[i]).append(" ");
             }
             sb.append("\n");
         } else {
-            for (int i = 1; i <= n; i++ ) {
-                if (visited[i] != 0) continue;
+            for (int i = 1; i <= n; i++) {
+                if (visited[i] == 1) continue;
                 visited[i] = 1;
                 selected[idx] = i;
                 dfs(idx + 1);

61d7df09fb27f9feda21e5a44e5cc8f5a2fe181c
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-02-04 00:07:21 +0900

    [Gold IV] Title: N-Queen, Time: 5172 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,20 +1,18 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
 
-    static int[] map;
-    static int n, q, answer;
-
+    static int n, answer;
+    static int[] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        q = n;
-        map = new int[n];
+
+        arr = new int[n];
 
         dfs(0);
 
-        System.out.print(answer);
+        System.out.println(answer);
     }
 
     static void dfs(int row) {
@@ -23,23 +21,24 @@ public class Main {
         } else {
             for (int col = 0; col < n; col++) {
                 if (isAttack(row, col)) {
-                    map[row] = col;
+                    arr[row] = col;
                     dfs(row + 1);
-                    map[row] = 0;
+                    arr[row] = 0;
                 }
             }
         }
     }
 
-    static boolean isAttack (int row, int col) {
+    static boolean isAttack(int row, int col) {
         for (int i = 0; i < row; i++) {
-            // 같은 행인지 검사
-            if (map[i] == col) return false;
-            // ↘️(우하향) 대각선 검사
-            if (row + col ==  i + map[i]) return false;
-            // ↖️(좌상향) 대각선 검사
-            if (row - col ==  i - map[i]) return false;
-            // if (Math.abs(row - i) == Math.abs(col - map[i])) 대각선검사
+            // 열 확인
+            if (arr[i] == col) return false;
+
+            // 대각선 확인
+            if (row + col == i + arr[i]) return false;
+
+            // 대각선 확인
+            if (row - col == i - arr[i]) return false;
         }
         return true;
     }

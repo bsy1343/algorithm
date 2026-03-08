6153ea04d617955733ba8def4686b2d86b10544f
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-09 00:40:04 +0900

    [Gold IV] Title: 고냥이, Time: 276 ms, Memory: 20372 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -4,37 +4,39 @@ import java.util.*;
 public class Main {
 
     static int n, answer, kind;
-    static int[] visited;
     static char[] arr;
+    static int[] visited;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
-        n = Integer.parseInt(br.readLine());
-        arr = br.readLine().toCharArray();
+        n = sc.nextInt();
+        arr = sc.next().toCharArray();
         visited = new int[26];
 
         int l = 0;
+        int answer = Integer.MIN_VALUE;
+
         for (int r = 0; r < arr.length; r++) {
             add(arr[r]);
+
             while(kind > n) {
                 remove(arr[l]);
                 l++;
             }
             answer = Math.max(answer, r - l + 1);
-
         }
 
         System.out.println(answer);
     }
 
     static void add(int val) {
-        visited[val - 'a']++;
-        if (visited[val - 'a'] == 1) kind++;
+        visited[val-'a']++;
+        if (visited[val-'a'] == 1) kind++;
     }
 
     static void remove(int val) {
-        visited[val - 'a']--;
-        if (visited[val - 'a'] == 0) kind--;
+        visited[val-'a']--;
+        if (visited[val-'a'] == 0) kind--;
     }
 }

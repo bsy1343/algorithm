ce7cc9cdf5cba9855946b11b030ad73eb87d4086
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-08 22:56:28 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 264 ms, Memory: 25792 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -4,23 +4,24 @@ import java.util.*;
 public class Main {
 
     static int n;
-    static long answer;
     static int[] arr, visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+
         arr = new int[n];
         visited = new int[100001];
-
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int l = 0;
+        long answer = 0;
         for (int r = 0; r < n; r++) {
             while (visited[arr[r]] != 0) {
                 visited[arr[l]]--;

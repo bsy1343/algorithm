701adbb72ab246460a4ffbdff366a06719f8cd54
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-08 19:12:46 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 24096 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,33 +4,29 @@ import java.util.*;
 public class Main {
     static int n, s;
     static int[] arr;
-
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
         arr = new int[n];
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int l = 0, r = 0;
-        int sum = 0;
-        int answer = Integer.MAX_VALUE;
-        while (true) {
-            if (sum >= s) {
-                answer = Math.min(answer, r - l);
-                sum -= arr[l++];
-            } else {
-                if (r == n) break;
-                sum += arr[r++];
+        
+        int l = 0, sum = 0, answer = Integer.MAX_VALUE;
+        for (int r = 0; r < n; r++) {
+            sum += arr[r];  // r 위치 원소 추가
+            
+            while (sum >= s) {  // 조건을 만족하는 동안
+                answer = Math.min(answer, r - l + 1);  // 답 갱신
+                sum -= arr[l];  // l 위치 원소 제거
+                l++;  // l 증가
             }
         }
+        
         System.out.println(answer == Integer.MAX_VALUE ? 0 : answer);
-
     }
 }

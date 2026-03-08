1c0c32425acdd1f99e123c5a237e5d874cbc6685
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-08 22:47:51 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 264 ms, Memory: 25568 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -3,32 +3,33 @@ import java.util.*;
 
 public class Main {
 
+    static int n;
+    static long answer;
+    static int[] arr, visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        // 1 <= n <= 100000
-        int[] visited = new int[100001];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        visited = new int[100001];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        long answer = 0;
-        for (int l = 0, r = -1; l < n; l++) {
-            // r을 옮길 수 있는 만큼 옮긴다.
-            while (r + 1 < n && visited[arr[r+1]] == 0) {
-                r++;
-                visited[arr[r]]++;
+        int l = 0;
+        for (int r = 0; r < n; r++) {
+            while (visited[arr[r]] != 0) {
+                visited[arr[l]]--;
+                l++;
             }
-
-            // 정답을 갱신한다.
+            visited[arr[r]]++;
             answer += r - l + 1;
-
-            // l을 옮겨주면서 arr[l]의 개수를 감소시킨다.
-            visited[arr[l]]--;
         }
+
         System.out.println(answer);
     }
 }

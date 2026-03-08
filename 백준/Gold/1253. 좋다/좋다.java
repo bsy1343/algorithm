395322e8e5235cddda67762a7e37fe36137a9b78
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-08 23:53:19 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,46 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, answer;
+
+    static int n;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
         arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         Arrays.sort(arr);
 
+        int answer = 0;
         for (int i = 0; i < n; i++) {
             int target = arr[i];
 
             int l = 0, r = n-1;
+            // l과 r의 합을 알아야 하기때문에 중복은 제외
             while(l < r) {
-                if (i == l) {
+                // target이 음수일 수도 있기 때문에 l도++를 해야 함
+                if (l == i) {
                     l++;
                     continue;
                 }
-                if (i == r) {
+                if (r == i) {
                     r--;
                     continue;
                 }
 
                 int sum = arr[l] + arr[r];
-
-                if (sum == target) {
+                if (target == sum) {
                     answer++;
                     break;
-                } else if (sum > target) {
-                    r--;
-                } else {
+                } else if (target > sum) {
                     l++;
+                } else {
+                    r--;
                 }
             }
         }

c37cdb5f820367647515ee0f5482eb6cbc32cf0a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-12 21:12:54 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,17 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static class Node {
-        int x, y, s;
-        Node(int x, int y, int s) {
+        int x, y, dist;
+        Node (int x, int y, int dist) {
             this.x = x;
             this.y = y;
-            this.s = s;
+            this.dist = dist;
         }
     }
-    static int n, m, answer = Integer.MAX_VALUE;
-    static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    static int[][] map, visited;
+
+    static int n, m;
+    static int[][] visited, map;
+    static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,45 +23,43 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n + 1][m + 1];
-        visited = new int[n + 1][m + 1];
+        visited = new int[n][m];
+        map = new int[n][m];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 1; j <= m; j++) {
-                int x = str.charAt(j-1) - '0';
-                map[i][j] = x;
+            for (int j = 0; j < m; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
-        bfs(1, 1);
-
-        System.out.println(answer);
+        bfs(0, 0, 1);
     }
 
-    static void bfs(int x, int y) {
+    static void bfs(int x, int y, int dist) {
         Queue<Node> q = new LinkedList();
-        q.add(new Node(x, y, 1));
+        q.add(new Node(x, y, dist));
         visited[x][y] = 1;
 
         while(!q.isEmpty()) {
             Node node = q.poll();
 
-            if (node.x == n && node.y == m) {
-                answer = Math.min(answer, node.s);
+            if (node.x == n - 1 && node.y == m - 1) {
+                System.out.println(node.dist);
+                break;
             }
 
-            for (int i = 0; i < direction.length; i++) {
-                int nx = node.x + direction[i][0];
-                int ny = node.y + direction[i][1];
+            for (int i = 0; i < directions.length; i++) {
+                int nx = node.x + directions[i][0];
+                int ny = node.y + directions[i][1];
 
-                if (nx < 1 || ny < 1 || nx > n || ny > m) continue;
-                if (map[nx][ny] != 1) continue;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
                 if (visited[nx][ny] != 0) continue;
+                    if (map[nx][ny] != 1) continue;
+
                 visited[nx][ny] = 1;
-                q.add(new Node(nx, ny, node.s + 1));
+                q.add(new Node(nx, ny, node.dist + 1));
             }
         }
-
     }
 }

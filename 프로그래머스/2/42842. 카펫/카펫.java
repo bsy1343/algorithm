f5c693c0d1a79961882a9d1ea9d528422e97ee4c
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-15 01:34:07 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,25 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
-    
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int total = brown + yellow;
         
-        for (int i = 3; i <= total; i++) {
-            if (total % i != 0) continue;
+        for (int h = 3; h <= total; h++) {
+            if (total % h != 0) continue;
             
-            int garo = total / i;        
-            int count = (garo - 2) * (i -2);
+            int w = total / h;
+            int count = (w-2) * (h-2);
             
             if (yellow == count) {
-                answer[0] = garo;
-                answer[1] = i;
+                answer[0] = w;
+                answer[1] = h;
                 break;
             }
-        
         }
+        
         return answer;
     }
 }

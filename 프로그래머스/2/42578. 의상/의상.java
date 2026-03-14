fc204c8cfeea9a22c2a146b7dd524c9599383317
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-14 18:33:19 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap();
         
         for (String[] str : clothes) {
             map.put(str[1], map.getOrDefault(str[1], 1) + 1);
         }
         
-        int answer = 1;
-        for (int val : map.values()) {
-            answer *= val;
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= entry.getValue();
         }
-        return answer - 1;
+        
+        return answer-1;
     }
 }

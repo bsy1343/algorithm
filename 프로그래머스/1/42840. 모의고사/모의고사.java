228be8f068d0539e5c634b5d2bd4e16cdd8d660a
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-15 00:35:22 +0900

    [level 1] Title: 모의고사, Time: 2.74 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        int[][] humans = {
+        int[][] human = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
+        
         int[] result = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == humans[0][i % humans[0].length]) result[0]++;
-            if (answers[i] == humans[1][i % humans[1].length]) result[1]++;
-            if (answers[i] == humans[2][i % humans[2].length]) result[2]++;
+            if (answers[i] == human[0][i % human[0].length]) result[0]++;
+            if (answers[i] == human[1][i % human[1].length]) result[1]++;
+            if (answers[i] == human[2][i % human[2].length]) result[2]++;
         }
         
         int max = Math.max(result[0], Math.max(result[1], result[2]));
         
+        
         ArrayList<Integer> al = new ArrayList();
-        for (int i = 0; i < 3; i++) {
-            if (result[i] == max) al.add(i+1);
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == max) al.add(i + 1);
         }
+        
         Collections.sort(al);
+        
         return al.stream().mapToInt(o -> o).toArray();
     }
 }

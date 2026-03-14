80465fef2f002878c356baf56054c5c3b509430f
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-03-15 07:46:55 +0900

    [level 3] Title: 입국심사, Time: 78.69 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,30 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
         long answer = 0;
-        long min = 1;
-        long max = (long) times[times.length - 1] * n;
         
-        while(min <= max) {
-            long mid = (min + max) / 2;
-            
+        Arrays.sort(times);
+        long l = 1;
+        long r = (long) times[times.length - 1] * n;
+        
+        while(l <= r) {
+            long mid = (l + r) / 2;
             if (isCheck(mid, n, times)) {
                 answer = mid;
-                max = mid - 1;
+                r = mid - 1;
             } else {
-                min = mid + 1;
+                l = mid + 1;
             }
         }
         return answer;
     }
     
-    static boolean isCheck(long curTime, int n, int[] times) {
+    static boolean isCheck(long target, int n, int[] times) {
         long total = 0;
-        
         for (int time : times) {
-            total += curTime/time;
+            total += target / time;
             if (total >= n) return true;
         }
         return false;

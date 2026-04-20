7a38e17acef6ed6b394d212b6783d213cbc83572
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-20 20:21:37 +0900

    [level 3] Title: 베스트앨범, Time: 4.16 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,16 +1,16 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     
-    static class Node implements Comparable<Node>{
-        String genres;
-        int total, play, idx;
+    static class Node implements Comparable<Node> {
+        String genre;
+        int play, total, idx;
         
-        Node (String genres, int total, int play, int idx) {
-            this.genres = genres;
-            this.total = total;
+        Node (String genre, int play, int total, int idx) {
+            this.genre = genre;
             this.play = play;
+            this.total = total;
             this.idx = idx;
         }
         
@@ -20,12 +20,12 @@ class Solution {
             if (this.play != node.play) return node.play - this.play;
             if (this.idx != node.idx) return this.idx - node.idx;
             
-            return this.genres.compareTo(node.genres);
+            return this.genre.compareTo(node.genre);
         }
     }
     
-    
     public int[] solution(String[] genres, int[] plays) {
+        
         HashMap<String, Integer> map = new HashMap();
         ArrayList<Node> al = new ArrayList();
         
@@ -34,21 +34,21 @@ class Solution {
         }
         
         for (int i = 0; i < genres.length; i++) {
-            al.add(new Node(genres[i], map.get(genres[i]), plays[i], i));
+            al.add(new Node(genres[i], plays[i], map.get(genres[i]), i));
         }
         
         Collections.sort(al);
         
-        HashMap<String, Integer> map2 = new HashMap();
         ArrayList<Integer> al2 = new ArrayList();
+        HashMap<String, Integer> map2 = new HashMap();
         for (Node node : al) {
-            int cnt = map2.getOrDefault(node.genres, 0);
-            
+            int cnt = map2.getOrDefault(node.genre, 0);
             if (cnt < 2) {
                 al2.add(node.idx);
-                map2.put(node.genres, map2.getOrDefault(node.genres, 0) + 1);
+                map2.put(node.genre, cnt+1);
             }
         }
+        
         return al2.stream().mapToInt(o -> o).toArray();
     }
 }

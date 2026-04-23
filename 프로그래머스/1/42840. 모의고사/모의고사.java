9bf43d8ad0a20b46d0ca2f417e5fc310594eb3f4
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-23 21:27:10 +0900

    [level 1] Title: 모의고사, Time: 4.50 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,31 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] human = {
+        
+        int[][] arr = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
         
-        int[] result = new int[3];
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == human[0][i % human[0].length]) result[0]++;
-            if (answers[i] == human[1][i % human[1].length]) result[1]++;
-            if (answers[i] == human[2][i % human[2].length]) result[2]++;
-        }
-        
-        int max = Math.max(result[0], Math.max(result[1], result[2]));
-        
-        
         ArrayList<Integer> al = new ArrayList();
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] == max) al.add(i + 1);
+        int a = 0, b = 0, c = 0;
+        int max = 0;
+        for (int i = 0; i < answers.length; i++) {
+            a = arr[0][i % arr[0].length] == answers[i] ? a + 1 : a;
+            b = arr[1][i % arr[1].length] == answers[i] ? b + 1 : b;
+            c = arr[2][i % arr[2].length] == answers[i] ? c + 1 : c;
+            
+            max = Math.max(a, Math.max(b, c));
         }
         
-        Collections.sort(al);
+        if (a == max) al.add(1);
+        if (b == max) al.add(2);
+        if (c == max) al.add(3);
+        
         
         return al.stream().mapToInt(o -> o).toArray();
     }

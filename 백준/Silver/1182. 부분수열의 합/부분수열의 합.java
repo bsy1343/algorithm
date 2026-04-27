187471eae5eeed2221f684b26e9e4764e45906ca
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-28 08:17:15 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,39 +2,88 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, s, answer;
-    static int[] arr;
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-
-        arr = new int[n + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // s가 0인 경우 부분수열의 모든 합을 구하지 않았을떄 0이기 때문에 그 경우의 수를 없앤다.
-        if (s == 0) answer--;
-
-        dfs(1, 0);
-
-        System.out.println(answer);
+    static void input() {
+        N = scan.nextInt();
+        S = scan.nextInt();
+        nums = new int[N + 1];
+        for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
     }
 
-    static void dfs(int idx, int value) {
-        if (idx > n) {
-            if (value == s) {
-                answer++;
-            }
+    static int N, S, ans;
+    static int[] nums;
+    // k번째 원소를 포함시킬 지 정하는 함수
+    // value:= k-1 번째 원소까지 골라진 원소들의 합
+    static void rec_func(int k, int value) {
+        if (k == N + 1) {  // 부분 수열을 하나 완성 시킨 상태
+            // value 가 S 랑 같은 지 확인하기
+            if (value == S) ans++;
         } else {
-            dfs(idx + 1, value + arr[idx]);
-            dfs(idx + 1, value);
+            // k 번째 원소를 포함시킬 지 결정하고 재귀호출해주기
+            // Include
+            rec_func(k + 1, value + nums[k]);
+            // Not Include
+            rec_func(k + 1, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
+        rec_func(1, 0);
+        // ans 가 정말 "진 부분집합"만 다루는 지 확인하기
+        if (S == 0){
+            ans--;
+        }
+        System.out.println(ans);
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }

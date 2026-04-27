4033107dd5e1eb0ef9e048e834206146bda07e90
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-27 13:13:52 +0900

    [Silver IV] Title: 카드, Time: 348 ms, Memory: 37560 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -2,34 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static class Node implements Comparable<Node> {
         long num;
         int cnt;
+
         Node (long num, int cnt) {
             this.num = num;
             this.cnt = cnt;
         }
+
         @Override
         public int compareTo(Node node) {
             if (this.cnt != node.cnt) return node.cnt - this.cnt;
             return Long.compare(this.num, node.num);
         }
     }
-
     static int n;
+    static HashMap<Long, Integer> map = new HashMap();
     static ArrayList<Node> al = new ArrayList();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        HashMap<Long, Integer> map = new HashMap();
         for (int i = 0; i < n; i++) {
-            Long num = Long.parseLong(br.readLine());
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            long card = Long.parseLong(br.readLine());
+            map.put(card, map.getOrDefault(card, 0) + 1);
         }
 
-        for (Map.Entry<Long, Integer> entry : map.entrySet()) {
-            al.add(new Node(entry.getKey(), entry.getValue()));
+        for (Map.Entry<Long, Integer> map : map.entrySet()) {
+            al.add(new Node(map.getKey(), map.getValue()));
         }
 
         Collections.sort(al);

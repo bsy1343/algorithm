64ba72775bf4920fede83bc2ab7f07a92fe653ce
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-28 08:12:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -19,18 +19,22 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        // s가 0인 경우 부분수열의 모든 합을 구하지 않았을떄 0이기 때문에 그 경우의 수를 없앤다.
         if (s == 0) answer--;
+
         dfs(1, 0);
 
         System.out.println(answer);
     }
 
-    static void dfs(int idx, int result) {
-        if (n + 1 == idx) {
-            if (result == s) answer++;
+    static void dfs(int idx, int value) {
+        if (idx > n) {
+            if (value == s) {
+                answer++;
+            }
         } else {
-            dfs(idx + 1, result + arr[idx]);
-            dfs(idx + 1, result);
+            dfs(idx + 1, value + arr[idx]);
+            dfs(idx + 1, value);
         }
     }
 }

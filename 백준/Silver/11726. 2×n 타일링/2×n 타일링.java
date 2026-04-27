835b6f147c0bae07c4fc0748de1ba7ac3acf2980
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-27 13:23:17 +0900

    [Silver III] Title: 2×n 타일링, Time: 180 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,23 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int n;
-    static int[] dy;
+    static int[] dp;
 
-    public static void main (String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
-        dy = new int[1001];
 
-        dy[1] = 1;
-        dy[2] = 2;
-        dy[3] = 3;
+        dp = new int [10001];
 
-        for (int i = 4; i <= n; i++) {
-            dy[i] = (dy[i - 1] + dy[i - 2]) % 10007;
+        dp[1] = 1;
+        dp[2] = 2;
+        // dp[3] = 3;
+        // dp[4] = 5;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
         }
 
-        System.out.println(dy[n]);
+        System.out.println(dp[n]);
     }
 }

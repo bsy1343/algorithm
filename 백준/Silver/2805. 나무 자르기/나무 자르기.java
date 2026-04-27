4c71b3123f07f53547769514667505aca1d35dde
bsy1343 <46506719+bsy1343@users.noreply.github.com>
2026-04-27 13:35:23 +0900

    [Silver II] Title: 나무 자르기, Time: 900 ms, Memory: 119480 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int n, m, answer;
     static int[] arr;
 
@@ -11,24 +12,28 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            arr[i] = x;
         }
 
+        // 상한을 구하기 위해서이지만 위의 for문에서 상한을 찾는게 더 좋긴함.
         Arrays.sort(arr);
 
+        // 본문에 절단기 설정은 0~양수라고 나옴 arr[n-1]은 제일 높은 나무
         int l = 0, r = arr[n-1];
         while(l <= r) {
             int mid = (l + r) / 2;
 
-            if (ditermination(mid)) {
+            if (determination(mid)) {
                 answer = mid;
+                // 최대값 탐색
                 l = mid + 1;
             } else {
+                // 최소값 탐색
                 r = mid - 1;
             }
         }
@@ -36,10 +41,10 @@ public class Main {
         System.out.println(answer);
     }
 
-    static boolean ditermination(int val) {
+    static boolean determination(int val) {
         long sum = 0;
         for (int i = 0; i < n; i++) {
-            if (arr[i] > val) {
+            if(arr[i] > val) {
                 sum += arr[i] - val;
             }
         }

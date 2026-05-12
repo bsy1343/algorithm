035cbecc67ff12686bf4fb6f60f521fd72099dd2
Sangyeol Baek <bsy1343@naver.com>
2026-05-12 09:41:25 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int total = brown + yellow;
         
-        for (int h = 3; h <= total; h++) {
+        int total = brown + yellow;
+        for (int h = 3; h < total; h++) {
             if (total % h != 0) continue;
             int w = total / h;
             
-            if ((w-2) * (h-2) == yellow) {
-                answer[0] = Math.max(w, h);
-                answer[1] = Math.min(w, h);
+            if ((w - 2) * (h - 2) == yellow) {
+                answer[0] = w;
+                answer[1] = h;
+                break;
             }
         }
         return answer;

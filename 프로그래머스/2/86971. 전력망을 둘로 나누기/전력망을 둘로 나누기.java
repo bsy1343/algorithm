efa09f7ba2288950f2c2a8ba690b75e8202a52f6
Sangyeol Baek <bsy1343@naver.com>
2026-05-13 18:59:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.35 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,54 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-    static ArrayList<Integer>[] al;
-    
     public int solution(int n, int[][] wires) {
-        
-        al = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            al[i] = new ArrayList();
-        }
-        
-        for (int[] arr : wires) {
-            al[arr[0]].add(arr[1]);
-            al[arr[1]].add(arr[0]);
-        }
-        
-        int answer = n;
-        for (int[] arr : wires) {
-            al[arr[0]].remove(Integer.valueOf(arr[1]));
-            al[arr[1]].remove(Integer.valueOf(arr[0]));
-            
-            int cnt = bfs(arr[0]);
-            
-            answer = Math.min(answer, Math.abs(cnt - (n-cnt)));
-            
-            al[arr[0]].add(arr[1]);
-            al[arr[1]].add(arr[0]);
-        }
-        
+        int answer = -1;
         return answer;
-    }
-
-    static int bfs(int start) {
-        int[] visited = new int[al.length + 1];
-        Queue<Integer> q = new LinkedList();
-        q.add(start);
-        visited[start] = 1;
-        
-        int cnt = 0;
-        while(!q.isEmpty()) {
-            int now = q.poll();
-            
-            cnt++;
-            for (int next : al[now]) {
-                if (visited[next] == 1) continue;
-                visited[next] = 1;
-                q.add(next);
-            }
-        }
-        return cnt;
     }
 }

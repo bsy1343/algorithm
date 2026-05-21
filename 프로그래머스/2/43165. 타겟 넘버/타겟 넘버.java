01feb3d1bbbc72124c3ebb7c36ed47a69e9ef170
Sangyeol Baek <bsy1343@naver.com>
2026-05-21 20:47:24 +0900

    [level 2] Title: 타겟 넘버, Time: 3.08 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
-    static int answer;
+    static int[] numbers;
+    static int target, answer;
     public int solution(int[] numbers, int target) {
-        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        this.answer = 0;
         
-        dfs(0, 0, numbers, target);
+        dfs(0, 0);
+        
         return answer;
     }
     
-    static void dfs(int idx, int result, int[] numbers, int target) {
+    static void dfs(int idx, int result) {
         if (idx == numbers.length) {
             if (result == target) answer++;
         } else {
-            dfs(idx + 1, result - numbers[idx], numbers, target);
-            dfs(idx + 1, result + numbers[idx], numbers, target);
+            dfs(idx + 1, result + numbers[idx]);
+            dfs(idx + 1, result - numbers[idx]);
         }
     }
 }

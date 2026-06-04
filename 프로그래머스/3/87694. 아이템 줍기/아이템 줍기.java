433ff95e522a0c4a478f7a41339c9a041607a5d5
Sangyeol Baek <bsy1343@naver.com>
2026-06-04 21:12:49 +0900

    [level 3] Title: 아이템 줍기, Time: 1.03 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -3,30 +3,30 @@ import java.util.*;
 
 class Solution {
     static class Node {
-        int x, y, dist;
-        Node (int x, int y, int dist) {
+        int x, y, d;
+        
+        Node (int x, int y, int d) {
             this.x = x;
             this.y = y;
-            this.dist = dist;
+            this.d = d;
         }
     }
     
-    static int n, m, answer;
-    static int[][] visited, map;
+    static int[][] map, visited;
     static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int n, answer;
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        n = rectangle.length;
-        m = rectangle[0].length;
+        this.n = rectangle.length;
         visited = new int[101][101];
         map = new int[101][101];
+        
         
         for (int i = 0; i < n; i++) {
             int ax = rectangle[i][0] * 2;
             int ay = rectangle[i][1] * 2;
             int bx = rectangle[i][2] * 2;
             int by = rectangle[i][3] * 2;
-            
             for (int x = ax; x <= bx; x++) {
                 for (int y = ay; y <= by; y++) {
                     map[x][y] = 1;
@@ -39,7 +39,6 @@ class Solution {
             int ay = rectangle[i][1] * 2;
             int bx = rectangle[i][2] * 2;
             int by = rectangle[i][3] * 2;
-            
             for (int x = ax+1; x < bx; x++) {
                 for (int y = ay+1; y < by; y++) {
                     map[x][y] = 0;
@@ -47,7 +46,7 @@ class Solution {
             }
         }
         
-        bfs(rectangle, characterX*2, characterY*2, itemX*2, itemY*2);
+        bfs(rectangle, characterX * 2, characterY * 2, itemX * 2, itemY * 2);
         
         return answer;
     }
@@ -58,23 +57,24 @@ class Solution {
         visited[characterX][characterY] = 1;
         
         while(!q.isEmpty()) {
-            Node node = q.poll();
+            Node now = q.poll();
             
-            if (node.x == itemX && node.y == itemY) {
-                answer = node.dist / 2;
+            if (now.x == itemX && now.y == itemY) {
+                answer = now.d / 2;
                 return;
             }
             
             for (int i = 0; i < directions.length; i++) {
-                int dx = node.x + directions[i][0];
-                int dy = node.y + directions[i][1];
+                int dx = now.x + directions[i][0];
+                int dy = now.y + directions[i][1];
                 
                 if (dx < 0 || dx >= 101 || dy < 0 || dy >= 101) continue;
-                if (visited[dx][dy] != 0) continue;
-                if (map[dx][dy] != 1) continue;
+                if (visited[dx][dy] == 1) continue;
+                if (map[dx][dy] == 0) continue;
                 visited[dx][dy] = 1;
-                q.add(new Node(dx, dy, node.dist + 1));
+                q.add(new Node(dx, dy, now.d + 1));
             }
         }
     }
+    
 }
